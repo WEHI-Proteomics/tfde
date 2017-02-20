8e3fe36ec5961d21599524da9563614cadeb155c
Daryl Wilding-McBride <dwm@dius.com.au>
2017-02-21 10:30:29 +1100

    Basic subset plot working.

--- subset.py
+++ subset.py
@@ -1,9 +1,8 @@
 import sys
 import numpy as np
-from numpy import genfromtxt
 import matplotlib.pyplot as plt
 from scipy.ndimage import gaussian_filter
-import csv
+import pandas as pd
 
 FRAME_ID = 30000
 
@@ -17,44 +16,16 @@ IMAGE_Y_PIXELS = 900
 
 
 # Read in the frames CSV
-rows = genfromtxt('frames-30000-30010.csv', delimiter=',')
+rows_df = pd.read_csv("frames-30000-30010.csv")
 
 # Create a subset for m/z 560-582, scans 300-650
 #   0      1    2    3     4
 # frame ID,mz,index,scan,intensity
+subset = rows_df[(rows_df.frame == FRAME_ID) & (rows_df.mz >= LOW_MZ) & (rows_df.mz <= HIGH_MZ) & (rows_df.scan >= LOW_SCAN) & (rows_df.scan <= HIGH_SCAN)]
 
-MIN_INDEX = sys.maxint
-MAX_INDEX = 0
-MIN_SCAN = sys.maxint
-MAX_SCAN = 0
-
-# Find the lowest and highest index and scan values
-for idx,row in enumerate(rows):
-	if idx > 0:
-		ind = int(row[2])
-		MIN_INDEX = min(MIN_INDEX, ind)
-		MAX_INDEX = max(MAX_INDEX, ind)
-		scan = int(row[3])
-		MIN_SCAN = min(MIN_SCAN, scan)
-		MAX_SCAN = max(MAX_SCAN, scan)
-
-print("max index = {}, min index = {}".format(MAX_INDEX, MIN_INDEX))
-print("max scan = {}, min scan = {}".format(MAX_SCAN, MIN_SCAN))
-
-frame = np.zeros((MAX_INDEX+1, MAX_SCAN+1))
-
-for idx,row in enumerate(rows):
-	if idx > 0:
-		frame_id = int(row[0])
-		mz = float(row[1])
-		index = int(row[2])
-		scan = int(row[3])
-		intensity = int(row[4])
-		if frame_id == FRAME_ID:
-			frame[index,scan] = intensity
 
 # Apply a Gaussian filter
-frame_g = gaussian_filter(frame, sigma=(10,10), order=0)
+# frame_g = gaussian_filter(frame, sigma=(10,10), order=0)
 
 # Plot the frame
 fig = plt.figure()
@@ -62,8 +33,9 @@ dpi = fig.get_dpi()
 fig.set_size_inches(float(IMAGE_X_PIXELS)/float(dpi), float(IMAGE_Y_PIXELS)/float(dpi))
 axes = fig.add_subplot(111)
 
-axes.imshow(frame_g, interpolation='nearest', cmap='hot', aspect='auto')
+axes.scatter(subset.mz, subset.scan)
 ax = plt.gca()
 ax.set_ylim(ax.get_ylim()[::-1])
+ax.axis('tight')
 plt.tight_layout()
 plt.show()
