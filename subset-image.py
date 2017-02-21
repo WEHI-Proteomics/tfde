3e60e244ef8d73f7206c72b71173a491d7523432
Daryl Wilding-McBride <dwm@dius.com.au>
2017-02-21 14:55:23 +1100

    Represent intensity with colour in an image.

---
+++ subset-image.py
@@ -0,0 +1,43 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.ndimage import gaussian_filter
+import pandas as pd
+
+FRAME_ID = 30000
+
+LOW_MZ = 565
+HIGH_MZ = 570
+LOW_SCAN = 500
+HIGH_SCAN = 600
+
+IMAGE_X_PIXELS = 1600
+IMAGE_Y_PIXELS = 900
+
+# Read in the frames CSV
+rows_df = pd.read_csv("frames-30000-30010.csv")
+
+# Create a subset
+subset = rows_df[(rows_df.frame == FRAME_ID) & (rows_df.mz >= LOW_MZ) & (rows_df.mz <= HIGH_MZ) & (rows_df.scan >= LOW_SCAN) & (rows_df.scan <= HIGH_SCAN)]
+
+x = subset.index - subset.index.min() # offset so we only get the view we want
+y = subset.scan
+subset_i = np.zeros((x.max()+1, y.max()+1))
+subset_i[x, y] = subset.intensity
+
+# Apply a Gaussian filter
+# subset_g = gaussian_filter(subset_array, sigma=(10,10), order=0)
+
+# Plot the frame
+fig = plt.figure()
+dpi = fig.get_dpi()
+fig.set_size_inches(float(IMAGE_X_PIXELS)/float(dpi), float(IMAGE_Y_PIXELS)/float(dpi))
+
+axes = fig.add_subplot(111)
+axes.imshow(subset_i, interpolation='nearest', cmap='hot')
+
+ax = plt.gca()
+ax.set_ylim(ax.get_ylim()[::-1])
+ax.axis('tight')
+plt.tight_layout()
+plt.show()
