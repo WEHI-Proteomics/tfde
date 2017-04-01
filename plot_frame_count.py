2ce349c557af34860fcf1b3818d0ee5112f0c82f
Daryl WM <daryl.wmcb@gmail.com>
2017-04-02 07:55:08 +1000

    Added labels.

--- plot_frame_count.py
+++ plot_frame_count.py
@@ -11,6 +11,9 @@ series = rows_df[['frame_id','points']].values
 fig = plt.figure()
 axes = fig.add_subplot(111)
 
-axes.plot(series[:,0], series[:,1], 'o', color='orange', linewidth=1, markerfacecolor='orange', markeredgecolor='black', markeredgewidth=0.0, markersize=1)
+axes.plot(series[:,0], series[:,1], '-', color='orange', linewidth=1, markerfacecolor='orange', markeredgecolor='black', markeredgewidth=0.0, markersize=1)
+plt.title('Number of data points above threshold (85) by frame')
+plt.xlabel('Frame')
+plt.ylabel('Number of points above threshold')
 plt.show()
 plt.close('all')
