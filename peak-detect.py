1ac3c677fb2bc10fc0f5d6f7f384abb194698d8b
Daryl WM <daryl.wmcb@gmail.com>
2017-05-21 08:23:08 +1000

    Record the point IDs in a peak.

--- peak-detect.py
+++ peak-detect.py
@@ -8,7 +8,7 @@ import peakutils
 import copy
 import sqlite3
 
-# Process a whole frame using DBSCAN, and then plot only the area of interest.
+# Process a set of frames using DBSCAN, and save the detected peaks to a database
 
 FRAME_START = 29900
 FRAME_END = 30100
@@ -59,8 +59,14 @@ c = conn.cursor()
 
 # Set up the table for detected peaks
 c.execute('''DROP TABLE IF EXISTS peaks''')
-c.execute('''CREATE TABLE peaks (peak_id INTEGER, frame_id INTEGER, low_mz REAL, high_mz REAL, low_scan INTEGER, high_scan INTEGER, state TEXT)''')
-c.execute('''CREATE INDEX idx_peaks ON peaks (peak_id, frame_id)''')
+c.execute('''DROP TABLE IF EXISTS peak_points''')
+c.execute('''CREATE TABLE peaks (frame_id INTEGER, peak_id INTEGER, state TEXT)''')
+c.execute('''CREATE TABLE peak_points (frame_id INTEGER, peak_id INTEGER, point_id INTEGER, PRIMARY KEY (frame_id, peak_id, point_id))''')
+# Indexes
+c.execute('''DROP INDEX IF EXISTS idx_peaks''')
+c.execute('''DROP INDEX IF EXISTS idx_peak_points''')
+c.execute('''CREATE INDEX idx_peaks ON peaks (frame_id, peak_id)''')
+c.execute('''CREATE INDEX idx_peak_points ON peak_points (frame_id, peak_id)''')
 
 for frame_id in range(FRAME_START, FRAME_END+1):
 
@@ -87,56 +93,22 @@ for frame_id in range(FRAME_START, FRAME_END+1):
 
     # Process the clusters
     mono_peaks = []  # where we record all the monoisotopic peaks we find
+    peak_points = []
     clusters = [X_pretransform[labels == i] for i in xrange(n_clusters_)]
     peak_id = 0
-    frame_id = frame_id
     for cluster in clusters:
-
         peak_id += 1
-
-        # find the bounding box of each cluster
-        bb = bbox(cluster)
-        x1 = bbox_points(bb)['tl']['x']
-        y1 = bbox_points(bb)['tl']['y']
-        x2 = bbox_points(bb)['br']['x']
-        y2 = bbox_points(bb)['br']['y']
-
-        # get the intensity values by scan
-        cluster_df = frame_df[(frame_df.mz >= x1) & (frame_df.mz <= x2) & (frame_df.scan >= y1) & (frame_df.scan <= y2)]
-        cluster_intensity = cluster_df[['scan','intensity']].values
-        cluster_mz = cluster_df[['mz','scan']].values
-
-        # filter the intensity with a Gaussian filter
-        window = signal.gaussian(20, std=5)
-        filtered = signal.convolve(cluster_intensity[:, 1], window, mode='same') / sum(window)
-
-        # find the maxima
-        indexes = peakutils.indexes(filtered, thres=0.1, min_dist=10)
-
-        # Find the minimum intensity between the maxima
-        if len(indexes) == 2:
-            minimum_intensity_index = np.argmin(filtered[indexes[0]:indexes[1]+1])+indexes[0]
-            # split the bounding box at the minima
-            a_peak, b_peak = split_bbox_y(bbox=bb, y=cluster_mz[minimum_intensity_index,1])
-            # draw a bounding box around a_peak
-            a_x1 = bbox_points(a_peak)['tl']['x']
-            a_y1 = bbox_points(a_peak)['tl']['y']
-            a_x2 = bbox_points(a_peak)['br']['x']
-            a_y2 = bbox_points(a_peak)['br']['y']
-            # draw a bounding box around b_peak
-            b_x1 = bbox_points(b_peak)['tl']['x']
-            b_y1 = bbox_points(b_peak)['tl']['y']
-            b_x2 = bbox_points(b_peak)['br']['x']
-            b_y2 = bbox_points(b_peak)['br']['y']
-            # add the peaks to the collection
-            mono_peaks.append((peak_id, frame_id, a_x1, a_x2, a_y1, a_y2, ""))
-            mono_peaks.append((peak_id, frame_id, b_x1, b_x2, b_y1, b_y2, ""))
-        else:
-            # add the peak to the collection
-            mono_peaks.append((peak_id, frame_id, x1, x2, y1, y2, ""))
-
+        # add the peak to the collection
+        mono_peaks.append((frame_id, peak_id, ""))
+        # write out all the points in the cluster
+        for point in cluster:
+            # Find the point's pointID
+            row = frame_df[(frame_df.mz == point[0]) & (frame_df.scan == point[1])]
+            point_id = int(row.point_id.values[0])
+            peak_points.append((frame_id, peak_id, point_id))
     # Write out all the peaks to the database
-    c.executemany("INSERT INTO peaks VALUES (?, ?, ?, ?, ?, ?, ?)", mono_peaks)
+    c.executemany("INSERT INTO peaks VALUES (?, ?, ?)", mono_peaks)
+    c.executemany("INSERT INTO peak_points VALUES (?, ?, ?)", peak_points)
     conn.commit()
 
 conn.close()
