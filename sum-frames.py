6b09ee6c0fdb51872db6aecd975f82ee4184585a
DarylWM <daryl.wmcb@gmail.com>
2017-06-29 16:39:01 +1000

    Fixed error in database output.

--- sum-frames.py
+++ sum-frames.py
@@ -7,13 +7,13 @@ import peakutils
 import matplotlib.pyplot as plt
 
 
-FRAME_START = 137000
-FRAME_END = 137005
+FRAME_START = 700
+FRAME_END = 720
 FRAMES_TO_SUM = 5
 INSTRUMENT_RESOLUTION = 40000.0
 MZ_INCREMENT = 0.00001
-SOURCE_SQLITE_FILE = "\\temp\\frames-20ms-th-0-137000-138000-V4.sqlite"
-DEST_SQLITE_FILE = "\\temp\\summed-frames-5-th-0-137000-138000-V4.sqlite"
+SOURCE_SQLITE_FILE = "\\temp\\161117-TIMS_TuneMix_MS_AutoMSMS-frames-th-0-1-1565-V4.sqlite"
+DEST_SQLITE_FILE = "\\temp\\summed-161117-TIMS_TuneMix_MS_AutoMSMS-frames-th-0-1-1565-V4.sqlite"
 
 # Formula from https://en.wikipedia.org/wiki/Gaussian_function
 def gaussian(x, amplitude, peak, stddev):
@@ -40,6 +40,7 @@ mz_min = frame_df.mz.min()
 mz_max = frame_df.mz.max()
 scan_min = frame_df.scan.min()
 scan_max = frame_df.scan.max()
+print("{} to {} m/z, {} to {} scan".format(mz_min, mz_max, scan_min, scan_max))
 
 vector_length = int((mz_max-mz_min)/MZ_INCREMENT)
 sum_vector = np.zeros(vector_length, dtype=np.float)
@@ -53,11 +54,11 @@ for base_frame in range(FRAME_START, FRAME_END, FRAMES_TO_SUM):
     # for scan in range(scan_min, scan_max+1):
     pointId = 0
     for scan in range(scan_min, scan_max+1):
-	    points = []
+        points = []
         print("  scan: {} of {}".format(scan, scan_max))
         for frame in range(base_frame, base_frame+FRAMES_TO_SUM):
-            print("    frame: {}".format(frame))
             points_df = frame_df[(frame_df.scan == scan) & (frame_df.frame_id == frame)].sort_values('mz', ascending=True)
+            print("    frame: {} ({} points on this scan)".format(frame, len(points_df)))
             for point_index in range(0,len(points_df)):
                 point = points_df.iloc[point_index]
                 stddev = (point.mz / INSTRUMENT_RESOLUTION) / 2.35482
@@ -71,27 +72,30 @@ for base_frame in range(FRAME_START, FRAME_END, FRAMES_TO_SUM):
         # sum_vector now contains the summed gaussians for the set of 5 frames for this scan line
 
         # Find the maxima for this scan
-        truncated_sum_vector = sum_vector[np.where(sum_vector > 1)[0]]
-        indexes = peakutils.indexes(truncated_sum_vector, thres=0.01, min_dist=1000)
+        signal_indices = np.where(sum_vector > 1)[0]
+        truncated_sum_vector = sum_vector[signal_indices]
+        if len(truncated_sum_vector) > 0:
+            print("    {} points in the truncated sum vector".format(len(truncated_sum_vector)))
+            indexes = peakutils.indexes(truncated_sum_vector, thres=0.01, min_dist=1000)
 
-        # Write out the maxima as points for this scan
-        for i in range(0,len(indexes)):
-            points.append((summedFrameId, pointId, mz_vector[i], scan, int(sum_vector[i]), 0))
-            pointId += 1
+            # Write out the maxima as points for this scan
+            for i in range(0,len(indexes)):
+                points.append((summedFrameId, pointId, mz_vector[signal_indices[indexes[i]]], scan, int(sum_vector[signal_indices[indexes[i]]]), 0))
+                pointId += 1
 
-        # reset the sum vector for the next scan
-        sum_vector.fill(0.0)
-
-	    print("Writing scan {} of frame {} ({} points) to the database.".format(scan, summedFrameId, len(points)))
-	    dest_c.executemany("INSERT INTO frames VALUES (?, ?, ?, ?, ?, ?)", points)
-	    dest_conn.commit()
+            print("    Writing scan {} of frame {} ({} points) to the database.".format(scan, summedFrameId, len(points)))
+            dest_c.executemany("INSERT INTO frames VALUES (?, ?, ?, ?, ?, ?)", points)
+            dest_conn.commit()
 
         # f = plt.figure()
         # ax1 = f.add_subplot(111)
         # plt.xlim(1050, 1150)
         # ax1.plot(mz_vector, sum_vector, 'o', markerfacecolor='orange', markeredgecolor='black', markeredgewidth=0.0, markersize=4)
-        # ax1.plot(mz_vector[np.where(sum_vector > 0)[0][indexes]], sum_vector[np.where(sum_vector > 0)[0][indexes]], 'o', markerfacecolor='red', markeredgecolor='black', markeredgewidth=0.0, markersize=6)
+        # ax1.plot(mz_vector[signal_indices[indexes]], sum_vector[signal_indices[indexes]], 'o', markerfacecolor='red', markeredgecolor='black', markeredgewidth=0.0, markersize=6)
         # plt.show()
+
+        # reset the sum vector for the next scan
+        sum_vector.fill(0.0)
 
     summedFrameId += 1
 
