3bc9782831f4461fd412825562b9b5de5b326d24
Daryl Wilding-McBride <wilding-mcbride.d@wehi.edu.au>
2017-12-16 09:30:08 +1100

    Updated.

--- generate-batch-sum-commands.py
+++ generate-batch-sum-commands.py
@@ -10,8 +10,11 @@ parser.add_argument('-ce','--collision_energy', type=int, default=10, help='Coll
 parser.add_argument('-sfb','--summed_frame_batch_size', type=int, default=10000, help='The number of summed frames in each batch.', required=False)
 args = parser.parse_args()
 
+args.source_directory = args.source_directory.replace('\\', '/')
+args.destination_directory = args.destination_directory.replace('\\', '/')
+
 # Connect to the database file to find out the number of unique frame ids for this collision energy
-source_db = "{}\{}".format(args.source_directory, args.base_database_name)
+source_db = "{}/{}".format(args.source_directory, args.base_database_name)
 print("connecting to {}".format(source_db))
 source_conn = sqlite3.connect(source_db)
 c = source_conn.cursor()
@@ -24,11 +27,16 @@ number_of_summed_frames = number_of_frames_in_source / args.frames_to_sum
 number_of_batches = number_of_summed_frames / args.summed_frame_batch_size
 if (number_of_summed_frames % args.summed_frame_batch_size > 0):
     number_of_batches += 1
-number_of_source_frames_per_batch = args.frames_to_sum * args.batch_size
+number_of_source_frames_per_batch = args.frames_to_sum * args.summed_frame_batch_size
 print("total summed frames {} in {} batches".format(number_of_summed_frames, number_of_batches))
 
-for i in range(args.number_of_batches):
-    start_frame_source = (i*number_of_source_frames_per_batch)+1
+for i in range(number_of_batches):
     start_frame_destination = i*args.summed_frame_batch_size+1
     end_frame_destination = (i+1)*args.summed_frame_batch_size
-    print("python \otf-peak-detect\sum-frames-intensity-descent.py -sdb {}\{} -ddb {}\summed-{}-{}-{} -n {} -bf {} -sf {}".format(args.source_directory, args.base_database_name, args.destination_directory, start_frame_destination, end_frame_destination, args.base_database_name, args.batch_size, start_frame_destination, start_frame_source))
+    if (end_frame_destination > number_of_summed_frames):
+    	end_frame_destination = number_of_summed_frames
+
+    start_frame_index_source = start_frame_destination-1
+    print("python sum-frames-intensity-descent.py -sdb \"{}/{}\" -ddb \"{}/summed-{}-{}-{}\" -n {} -bf {} -sf {}".format(args.source_directory, 
+    	args.base_database_name, args.destination_directory, start_frame_destination, end_frame_destination, args.base_database_name, args.summed_frame_batch_size, 
+    	start_frame_destination, start_frame_index_source))
