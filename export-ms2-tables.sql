3479d1327418926f1e13a5d93b09aaeb099962a5
Daryl Wilding-McBride <wilding-mcbride.d@wehi.edu.au>
2018-01-06 13:52:25 +1100

    Initial commit.

---
+++ export-ms2-tables.sql
@@ -0,0 +1,8 @@
+.mode csv
+.headers on
+.once "S:/data/Projects/ProtemicsLab/Bruker timsTOF/experiments/ms2-feature/summed-ms2-regions.csv"
+select * from summed_ms2_regions;
+.once "S:/data/Projects/ProtemicsLab/Bruker timsTOF/experiments/ms2-feature/ms1-features.csv"
+select * from features where feature_id=1;
+.once "S:/data/Projects/ProtemicsLab/Bruker timsTOF/experiments/ms2-feature/clusters.csv"
+select frame_id,cluster_id,charge_state,base_isotope_peak_id,base_peak_max_point_mz,base_peak_max_point_scan,fit_error,intensity_sum,feature_id,mz_lower,mz_upper,scan_lower,scan_upper from clusters where feature_id=1;
