9737b2d8692229074a154ed9070fb58f4f6491eb
Daryl Wilding-McBride <wilding-mcbride.d@wehi.edu.au>
2018-01-07 18:46:04 +1100

    Added ms2 peaks.

--- export-ms2-tables.sql
+++ export-ms2-tables.sql
@@ -6,3 +6,5 @@ select * from summed_ms2_regions;
 select * from features where feature_id=1;
 .once "S:/data/Projects/ProtemicsLab/Bruker timsTOF/experiments/ms2-feature/clusters.csv"
 select frame_id,cluster_id,charge_state,base_isotope_peak_id,base_peak_max_point_mz,base_peak_max_point_scan,fit_error,intensity_sum,feature_id,mz_lower,mz_upper,scan_lower,scan_upper from clusters where feature_id=1;
+.once "S:/data/Projects/ProtemicsLab/Bruker timsTOF/experiments/ms2-feature/ms2-peaks.csv"
+select * from ms2_peaks where feature_id=1;
