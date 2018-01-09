7be5b1d542bc360e163daa97fa51139327d9c46c
Daryl Wilding-McBride <wilding-mcbride.d@wehi.edu.au>
2018-01-09 18:44:04 +1100

    Updated.

--- export-ms2-tables.sql
+++ export-ms2-tables.sql
@@ -8,3 +8,5 @@ select * from features where feature_id=1;
 select frame_id,cluster_id,charge_state,base_isotope_peak_id,base_peak_max_point_mz,base_peak_max_point_scan,fit_error,intensity_sum,feature_id,mz_lower,mz_upper,scan_lower,scan_upper from clusters where feature_id=1;
 .once "S:/data/Projects/ProtemicsLab/Bruker timsTOF/experiments/ms2-feature/ms2-peaks.csv"
 select * from ms2_peaks where feature_id=1;
+.once "S:/data/Projects/ProtemicsLab/Bruker timsTOF/experiments/ms2-feature/summed-ms1-regions.csv"
+select * from summed_ms1_regions;
