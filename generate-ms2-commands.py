d078f9a2cb3f333658a09795f656e114aacec4d5
Daryl Wilding-McBride <wilding-mcbride.d@wehi.edu.au>
2018-01-25 10:02:44 +1100

    Updated.

--- generate-ms2-commands.py
+++ generate-ms2-commands.py
@@ -5,6 +5,7 @@ import pymysql
 parser = argparse.ArgumentParser(description='Generates the commands to run MS2 peak detection in parallel.')
 parser.add_argument('-db','--database_name', type=str, help='The name of the database.', required=True)
 parser.add_argument('-fbs','--number_of_batches', type=int, default=12, help='The number of batches.', required=False)
+parser.add_argument('-ms2ce','--ms2_collision_energy', type=float, help='Collision energy used for MS2.', required=True)
 args = parser.parse_args()
 
 # Connect to the database
@@ -36,7 +37,7 @@ for i in range(args.number_of_batches):
     # print("start \"Summing {}-{}\" python sum-frames-intensity-descent.py -sdb \"{}/{}\" -ddb \"{}/summed-{}-{}-{}\" -n {} -bf {} -sf {}".format(base_feature_id, last_summed_frame_id, args.source_directory, 
     #     args.base_database_name, args.destination_directory, base_feature_id, last_summed_frame_id, args.base_database_name, number_of_features_required_this_batch, 
     #     base_feature_id, base_source_frame_index))
-    print("nohup python -u ./feature-region-ms2-sum-frames.py -db {} -fl {} -fu {} > ../logs/{}-ms2-feature-region-sum-batch-{}-{}-{}.log 2>&1 &".format(args.database_name, first_feature_id, last_feature_id, args.database_name, i, first_feature_id, last_feature_id))
+    print("nohup python -u ./feature-region-ms2-sum-frames.py -db {} -fl {} -fu {} -ms2ce {} > ../logs/{}-ms2-feature-region-sum-batch-{}-{}-{}.log 2>&1 &".format(args.database_name, first_feature_id, last_feature_id, args.ms2_collision_energy, args.database_name, i, first_feature_id, last_feature_id))
 
 # print("")
 
