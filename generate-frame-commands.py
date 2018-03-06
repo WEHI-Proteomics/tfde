1d5e9da4523f207850c7c1ecaf494133ea80b3af
Daryl Wilding-McBride <dwm@dius.com.au>
2018-03-06 14:05:24 +1100

    Updated.

--- generate-frame-commands.py
+++ generate-frame-commands.py
@@ -53,16 +53,14 @@ for i in range(args.number_of_batches):
     # print("qsub -l nodes=1:ppn=12,mem=4gb -F \"./otf-peak-detect/sum-frames-ms1.py -db {} -fl {} -fu {} -ce {}\" ./py.sh".format(args.database_name, first_frame_id, last_frame_id, args.collision_energy))
 
 print("")
-print("nohup python -u ./otf-peak-detect/peak-detect-ms1-prep.py -db {} > ./logs/peak-detect-ms1-prep-{}.log 2>&1 &".format(args.database_name, args.database_name))
 for i in range(args.number_of_batches):
     first_frame_id = i*batch_size+1
     last_frame_id = first_frame_id + batch_size - 1
     if last_frame_id > number_of_frames:
         last_frame_id = number_of_frames
 
-    print("nohup python -u ./otf-peak-detect/peak-detect-ms1.py -sdb {}.sqlite -ddb {}-{}-{}-{}.sqlite "
+    print("nohup python -u ./otf-peak-detect/peak-detect-ms1.py -db {}-{}-{}-{}.sqlite "
         "-fl {} -fu {} > ./logs/peak-detect-ms1-{}-{}.log 2>&1 &".format(
-            args.database_name, 
             args.database_name, i, first_frame_id, last_frame_id, 
             first_frame_id, last_frame_id, 
             first_frame_id, last_frame_id))
--- peak-detect-ms1.py
+++ peak-detect-ms1.py
@@ -55,6 +55,19 @@ args = parser.parse_args()
 source_conn = sqlite3.connect(args.database_name)
 src_c = source_conn.cursor()
 
+print("Setting up tables...")
+
+src_c.execute("DROP TABLE IF EXISTS peaks")
+src_c.execute("DROP TABLE IF EXISTS peak_detect_info")
+
+src_c.execute("CREATE TABLE peaks (frame_id INTEGER, peak_id INTEGER, centroid_mz REAL, centroid_scan REAL, intensity_sum INTEGER, scan_upper INTEGER, scan_lower INTEGER, std_dev_mz REAL, std_dev_scan REAL, rationale TEXT, intensity_max INTEGER, peak_max_mz REAL, peak_max_scan INTEGER, cluster_id INTEGER, PRIMARY KEY (frame_id, peak_id))")
+src_c.execute("CREATE TABLE peak_detect_info (item TEXT, value TEXT)")
+
+print("Setting up indexes...")
+
+src_c.execute("CREATE INDEX IF NOT EXISTS idx_summed_frames ON summed_frames (frame_id)")
+src_c.execute("CREATE INDEX IF NOT EXISTS idx_summed_frames_2 ON summed_frames (frame_id,point_id)")
+
 if args.frame_lower is None:
     src_c.execute("SELECT value FROM summing_info WHERE item=\"frame_lower\"")
     row = src_c.fetchone()
