0dbd0adf02e7dbafa195ee0311d4835080c60b56
Daryl Wilding-McBride <dwm@dius.com.au>
2018-03-24 21:27:29 +1100

    Updated.

--- generate-feature-commands.py
+++ generate-feature-commands.py
@@ -3,7 +3,8 @@ import sqlite3
 
 # Process the command line arguments
 parser = argparse.ArgumentParser(description='Generates the commands to run MS2 peak detection in parallel.')
-parser.add_argument('-db','--database_name', type=str, help='The name of the database.', required=True)
+parser.add_argument('-sdb','--source_database_name', type=str, help='The name of the source database.', required=True)
+parser.add_argument('-ddb','--destination_database_name', type=str, help='The name of the destination database.', required=True)
 parser.add_argument('-fbs','--number_of_batches', type=int, default=12, help='The number of batches.', required=False)
 parser.add_argument('-ms2ce','--ms2_collision_energy', type=float, help='Collision energy used for MS2.', required=False)
 parser.add_argument('-ml','--mz_lower', type=float, help='Lower feature m/z to process.', required=False)
@@ -14,19 +15,22 @@ parser.add_argument('-nbf','--number_of_features', type=int, help='The number of
 args = parser.parse_args()
 
 # Connect to the database
-source_conn = sqlite3.connect(args.database_name)
+source_conn = sqlite3.connect(args.source_database_name)
 src_c = source_conn.cursor()
+
+dest_conn = sqlite3.connect(args.destination_database_name)
+dest_c = dest_conn.cursor()
 
 if args.number_of_features is None:
     # Find out the number of MS1 features in the database
-    src_c.execute("SELECT MAX(feature_id) FROM features")
-    row = src_c.fetchone()
+    dest_c.execute("SELECT MAX(feature_id) FROM features")
+    row = dest_c.fetchone()
     number_of_features = int(row[0])
 else:
     number_of_features = args.number_of_features
 
 # Close the database connection
-source_conn.close()
+dest_conn.close()
 
 batch_size = number_of_features / args.number_of_batches
 if (batch_size * args.number_of_batches) < number_of_features:
@@ -53,7 +57,7 @@ if (args.operation == 'all') or (args.operation == 'sum-ms2-region'):
                 print("start \"Summing {}-{}\" python -u feature-region-ms2-sum-frames.py -db {} -fl {} -fu {} -ms2ce {} -ml {} -mu {}"
                     .format(first_feature_id, last_feature_id, args.database_name, first_feature_id, last_feature_id, args.ms2_collision_energy, args.mz_lower, args.mz_upper))
             else:
-                print("nohup python -u ./otf-peak-detect/feature-region-ms2-sum-frames.py -db {}.sqlite -fl {} -fu {} -ms2ce {} -ml {} -mu {} > {}-ms2-feature-region-sum-batch-{}-{}-{}.log 2>&1 &"
+                print("nohup python -u ./otf-peak-detect/feature-region-ms2-sum-frames.py -sdb {}.sqlite -ddb {}-features.sqlite -fl {} -fu {} -ms2ce {} -ml {} -mu {} > {}-ms2-feature-region-sum-batch-{}-{}-{}.log 2>&1 &"
                     .format(args.database_name, first_feature_id, last_feature_id, args.ms2_collision_energy, args.mz_lower, args.mz_upper, args.database_name, i, first_feature_id, last_feature_id))
     else:
         print("ERROR: mandatory parameters missing.")
