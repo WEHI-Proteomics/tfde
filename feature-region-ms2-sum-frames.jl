88337f2820b7020a212b252fcc5a774bcdc4af5b
Daryl Wilding-McBride <dwm@dius.com.au>
2018-04-06 21:36:54 +1000

    No longer needed.

--- feature-region-ms2-sum-frames.jl
+++
@@ -1,6 +0,0 @@
-using SQLite
-
-conv_db = SQLite.DB("/Users/darylwilding-mcbride/Downloads/Hela_20A_20R_500-60-seconds-ms1-test.sqlite")
-source_db = SQLite.DB("/Users/darylwilding-mcbride/Downloads/Hela_20A_20R_500-60-seconds-ms1-test.sqlite")
-dest_db = SQLite.DB("/Users/darylwilding-mcbride/Downloads/Hela_20A_20R_500-60-seconds-ms1-test.sqlite")
-
