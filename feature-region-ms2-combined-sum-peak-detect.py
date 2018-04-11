aba8f4ec1eb6ff710845c644384ee8fc9f788b73
Daryl Wilding-McBride <dwm@dius.com.au>
2018-04-11 19:37:59 +1000

    Updated.

--- feature-region-ms2-combined-sum-peak-detect.py
+++ feature-region-ms2-combined-sum-peak-detect.py
@@ -211,7 +211,7 @@ def main():
                     peak_id += 1
 
                     # flag the points we've processed
-                    summed_intensities_by_mz[mzs] = 0
+                    summed_intensities_by_mz[mz] = 0
 
             feature_stop_time = time.time()
             feature_count += 1
