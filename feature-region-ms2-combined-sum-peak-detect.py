8b8c794d4158b90e06f4d804965fc35397f336d4
Daryl Wilding-McBride <dwm@dius.com.au>
2018-04-13 14:29:48 +1000

    Debug.

--- feature-region-ms2-combined-sum-peak-detect.py
+++ feature-region-ms2-combined-sum-peak-detect.py
@@ -191,7 +191,7 @@ def main():
             # sum the feature region intensities by mz (i.e. column)
             summed_intensities_by_mz = subset_frame_a.sum(axis=0)
             sorted_mzs = np.argsort(summed_intensities_by_mz)[::-1]
-            print("sorted_mzs {}-{}".format(np.min(sorted_mzs), np.max(sorted_mzs))
+            print("sorted_mzs {}-{}".format(np.min(sorted_mzs), np.max(sorted_mzs)))
             # check where we should stop
             zero_indices = np.where(summed_intensities_by_mz[sorted_mzs] == 0)[0]
             if len(zero_indices) > 0:
