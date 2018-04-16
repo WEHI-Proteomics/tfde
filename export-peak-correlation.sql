68555489ade42327901de00ab9b1709b07f1bd8c
Daryl Wilding-McBride <dwm@dius.com.au>
2018-04-16 20:28:07 +1000

    Changed order.

--- export-peak-correlation.sql
+++ export-peak-correlation.sql
@@ -1,8 +1,8 @@
 .mode csv
 .headers on
 .once "S:/data/Projects/ProtemicsLab/Bruker timsTOF/experiments/ms2-feature/correlation/summed_ms1_regions.csv"
-select * from summed_ms1_regions;
+select * from summed_ms1_regions order by feature_id;
 .once "S:/data/Projects/ProtemicsLab/Bruker timsTOF/experiments/ms2-feature/correlation/summed_ms2_regions.csv"
-select * from summed_ms2_regions;
+select * from summed_ms2_regions order by feature_id;
 .once "S:/data/Projects/ProtemicsLab/Bruker timsTOF/experiments/ms2-feature/correlation/peak_correlation.csv"
 select * from peak_correlation order by feature_id,base_peak_id,correlation;
