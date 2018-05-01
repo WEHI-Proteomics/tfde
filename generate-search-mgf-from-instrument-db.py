3e3523259b3cdc2161270edf6b600486890984d3
Daryl Wilding-McBride <dwm@dius.com.au>
2018-05-01 21:16:58 +1000

    Updated.

--- generate-search-mgf-from-instrument-db.py
+++ generate-search-mgf-from-instrument-db.py
@@ -2,8 +2,8 @@ import os
 from multiprocessing import Pool
 
 processes = (
-    'sum-frames-ms1.py -sdb /media/data-drive/Hela_20A_20R_500.sqlite -ddb test-Hela_20A_20R_500-1-5.sqlite -ce 7 -fl 1 -fu 5', 
-    'sum-frames-ms1.py -sdb /media/data-drive/Hela_20A_20R_500.sqlite -ddb test-Hela_20A_20R_500-6-10.sqlite -ce 7 -fl 6 -fu 10',
+    './otf-peak-detect/sum-frames-ms1.py -sdb /media/data-drive/Hela_20A_20R_500.sqlite -ddb test-Hela_20A_20R_500-1-5.sqlite -ce 7 -fl 1 -fu 5', 
+    './otf-peak-detect/sum-frames-ms1.py -sdb /media/data-drive/Hela_20A_20R_500.sqlite -ddb test-Hela_20A_20R_500-6-10.sqlite -ce 7 -fl 6 -fu 10',
     )
 # other = ('process3.py',)
 
