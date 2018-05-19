8b103c4099905a892cff5378e8b2b37ab7c5f221
Daryl Wilding-McBride <dwm@dius.com.au>
2018-05-19 15:17:14 +1000

    Updated.

--- feature-region-ms2-combined-sum-peak-detect-prep.py
+++ feature-region-ms2-combined-sum-peak-detect-prep.py
@@ -6,13 +6,9 @@ import sqlite3
 
 parser = argparse.ArgumentParser(description='Prepare the database for MS1 frame summing in the region of the MS1 feature\'s drift and retention time.')
 parser.add_argument('-cdb','--converted_database_name', type=str, help='The name of the converted database.', required=True)
-parser.add_argument('-sdb','--source_database_name', type=str, help='The name of the source database.', required=True)
 args = parser.parse_args()
 
 # Connect to the database
-src_conn = sqlite3.connect(args.source_database_name)
-src_c = src_conn.cursor()
-
 conv_conn = sqlite3.connect(args.converted_database_name)
 conv_c = conv_conn.cursor()
 
@@ -21,10 +17,7 @@ print("Setting up indexes")
 conv_c.execute("CREATE INDEX IF NOT EXISTS idx_frame_properties_2 ON frame_properties (collision_energy, frame_id)")
 conv_c.execute("CREATE INDEX IF NOT EXISTS idx_frames_2 ON frames (frame_id,scan)")
 
-src_c.execute("CREATE INDEX IF NOT EXISTS idx_features_1 ON features (feature_id,charge_state,mz_lower,mz_upper)")
+conv_c.execute("CREATE INDEX IF NOT EXISTS idx_features_1 ON features (feature_id,charge_state,mz_lower,mz_upper)")
 
 conv_conn.commit()
 conv_conn.close()
-
-src_conn.commit()
-src_conn.close()
