626c92d850e3537f6c8304cb6fff24c19c627137
Daryl Wilding-McBride <dwm@dius.com.au>
2018-06-08 11:55:40 +1000

    Updated.

--- correlate-ms2-peaks.py
+++ correlate-ms2-peaks.py
@@ -98,10 +98,10 @@ print("creating idx_peak_correlation_1")
 src_c.execute("CREATE INDEX IF NOT EXISTS idx_peak_correlation_1 on summed_ms1_regions (feature_id, peak_id)")
 print("creating idx_peak_correlation_2")
 src_c.execute("CREATE INDEX IF NOT EXISTS idx_peak_correlation_2 on feature_base_peaks (feature_id)")
+print("dropping idx_peak_correlation_3 if exists")
+src_c.execute("DROP INDEX IF EXISTS idx_peak_correlation_3")
 print("creating idx_peak_correlation_3")
 src_c.execute("CREATE INDEX IF NOT EXISTS idx_peak_correlation_3 on summed_ms2_regions (feature_id)")
-# print("dropping idx_peak_correlation_3 if exists")
-# src_c.execute("DROP INDEX IF EXISTS idx_peak_correlation_3")
 
 start_run = time.time()
 
