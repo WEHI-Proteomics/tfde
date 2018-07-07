465c9cb6966b63936f8b5856f8157ecfc1d52b64
Daryl Wilding-McBride <dwm@dius.com.au>
2018-07-08 09:15:05 +1000

    Drop the table once it's merged.

--- generate-search-mgf-from-instrument-db.py
+++ generate-search-mgf-from-instrument-db.py
@@ -37,6 +37,9 @@ def merge_summed_regions(source_db_name, destination_db_name, exceptions):
                 table_df = pd.read_sql_query(query, con=source_conn)
                 table_df.to_sql(name=table_name, con=destination_conn, if_exists='append', index=False, chunksize=None)
 
+            # drop the table in the source database
+            src_cur.execute('DROP TABLE IF EXISTS {table_name}'.format(table_name=table_name)
+
     source_conn.close()
     destination_conn.commit()
     destination_conn.close()
