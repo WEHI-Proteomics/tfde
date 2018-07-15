1440a837f33925a67daf0f8e6618fb643f6e5c6e
Daryl Wilding-McBride <dwm@dius.com.au>
2018-07-16 09:03:34 +1000

    Updated with processing time.

--- parallel-load-feature-databases.py
+++ parallel-load-feature-databases.py
@@ -1,50 +1,28 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 import glob
 import sqlite3
 import pandas as pd
 import os
 import multiprocessing as mp
 from multiprocessing import Pool
-
-
-# In[2]:
-
+import time
 
 def run_process(process):
     print("Executing: {}".format(process))
     os.system(process)
 
-
-# In[3]:
-
+processing_start_time = time.time()
 
 feature_database_name = "./UPS2_allion_Slot1-2_01_699/UPS2_allion_Slot1-2_01_699.sqlite"
-
-
-# In[4]:
-
-
 SOURCE_DATABASE_DIRECTORY = "./UPS2_allion_Slot1-2_01_699"
 file_list = glob.glob("{}/UPS2_allion_Slot1-2_01_699-features-*-*.sqlite".format(SOURCE_DATABASE_DIRECTORY))
-
-
-# In[5]:
-
 
 merge_processes = []
 for source_db in file_list:
     merge_processes.append("pgloader {} postgresql://dwm:password@recombine-test.ct0qrar1ezs6.us-west-1.rds.amazonaws.com/recombine_test".format(source_db))
 
-
-# In[ ]:
-
-
 # Set up the processing pool
 pool = Pool()
 pool.map(run_process, merge_processes)
 
+processing_stop_time = time.time()
+print("merge processing time: {} seconds".format(processing_stop_time-processing_start_time))
