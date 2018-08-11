acd1e648075f1035fd0ac9de19dce3089b324d2a
Daryl Wilding-McBride <dwm@dius.com.au>
2018-08-11 10:32:35 +1000

    Updated.

--- create-csv-for-top-ms2-peaks-of-the-top-features.py
+++ create-csv-for-top-ms2-peaks-of-the-top-features.py
@@ -9,8 +9,10 @@ DB_NAME = '/home/ubuntu/UPS2_allion/UPS2_allion-features-1-1097.sqlite'
 CONV_DB_NAME = '/home/ubuntu/UPS2_allion/UPS2_allion.sqlite'
 
 parser = argparse.ArgumentParser(description='Extract a CSV containing the top ms2 peaks for the top features.')
-parser.add_argument('-mrtd','--maximum_rt_delta_tolerance', type=float, help='The maximum RT delta tolerance.', required=True)
-parser.add_argument('-msd','--maximum_scan_delta_tolerance', type=float, help='The maximum scan delta tolerance.', required=True)
+parser.add_argument('-nrtd','--negative_rt_delta_tolerance', type=float, help='The negative RT delta tolerance.', required=True)
+parser.add_argument('-prtd','--positive_rt_delta_tolerance', type=float, help='The positive RT delta tolerance.', required=True)
+parser.add_argument('-nsd','--negative_scan_delta_tolerance', type=float, help='The negative scan delta tolerance.', required=True)
+parser.add_argument('-psd','--positive_scan_delta_tolerance', type=float, help='The positive scan delta tolerance.', required=True)
 parser.add_argument('-mnf','--maximum_number_of_features', type=int, help='The maximum number of features.', required=True)
 parser.add_argument('-mnp','--maximum_number_of_peaks_per_feature', type=int, help='The maximum number of peaks per feature.', required=True)
 parser.add_argument('-of','--output_filename', type=str, help='The output CSV filename.', required=True)
@@ -32,10 +34,11 @@ if os.path.isfile(args.output_filename):
 for idx in range(len(top_features_df)):
     feature_id = top_features_df.loc[idx].feature_id
     print("feature ID {}".format(feature_id))
-    df_1 = pd.read_sql_query("select feature_id,peak_id,centroid_mz,intensity from ms2_peaks where feature_id || '-' || peak_id in (select feature_id || '-' || ms2_peak_id from peak_correlation where feature_id == {} and abs(rt_distance) <= {} and abs(scan_distance) <= {} order by ms2_peak_id limit {})".format(feature_id, args.maximum_rt_delta_tolerance, args.maximum_scan_delta_tolerance, args.maximum_number_of_peaks_per_feature), db_conn)
-    df_2 = pd.read_sql_query("select * from peak_correlation where feature_id=={} and abs(rt_distance) <= {} and abs(scan_distance) <= {} order by ms2_peak_id limit {}".format(feature_id, args.maximum_rt_delta_tolerance, args.maximum_scan_delta_tolerance, args.maximum_number_of_peaks_per_feature), db_conn)
-    df = pd.merge(df_1, df_2, left_on=['feature_id','peak_id'], right_on=['feature_id','ms2_peak_id'])
-    df.drop(['peak_id','correlation'], inplace=True, axis=1)
+    peak_correlation_df = pd.read_sql_query("select * from peak_correlation where feature_id=={} and rt_distance >= {} and rt_distance <= {} and scan_distance >= {} and scan_distance <= {} order by rt_distance ASC limit {}".format(feature_id, args.negative_rt_delta_tolerance, args.positive_rt_delta_tolerance, args.negative_scan_delta_tolerance, args.positive_scan_delta_tolerance, args.maximum_number_of_peaks_per_feature), db_conn)
+    peak_correlation_df["feature_id-ms2_peak_id"] = peak_correlation_df.feature_id.astype(str) + '-' + peak_correlation_df.ms2_peak_id.astype(str)
+    ms2_peaks_df = pd.read_sql_query("select feature_id,peak_id,centroid_mz,intensity from ms2_peaks where feature_id || '-' || peak_id in {}".format(tuple(peak_correlation_df["feature_id-ms2_peak_id"])), db_conn)
+    df = pd.merge(ms2_peaks_df, peak_correlation_df, left_on=['feature_id','peak_id'], right_on=['feature_id','ms2_peak_id'])
+    df.drop(['peak_id','correlation','feature_id-ms2_peak_id'], inplace=True, axis=1)
     df.rename(columns={'intensity': 'ms2_peak_intensity', 'rt_distance': 'rt_delta', 'scan_distance': 'scan_delta', 'centroid_mz': 'ms2_peak_centroid_mz'}, inplace=True)
     # write the CSV
     if os.path.isfile(args.output_filename):
