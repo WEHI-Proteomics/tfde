d113269569ab83e1ebca55f7a5795ea7730a71fb
Daryl Wilding-McBride <dwm@dius.com.au>
2018-08-28 07:23:51 +1000

    Changed to the post-HK ms2 peaks.

--- create-csv-for-top-ms2-peaks-for-each-feature.py
+++ create-csv-for-top-ms2-peaks-for-each-feature.py
@@ -46,10 +46,10 @@ for idx in range(len(feature_ids_df)):
     feature_id = feature_ids_df.loc[idx].feature_id
     print("feature ID {}".format(feature_id), end="")
 
-    # merge the ms2 peaks with the peak correlation information
+    # merge the deconvolved/deisotoped ms2 peaks (i.e. after HK) with the peak correlation information
     peak_correlation_df = pd.read_sql_query("select * from peak_correlation where feature_id=={} and rt_distance >= {} and rt_distance <= {} and scan_distance >= {} and scan_distance <= {} order by ms2_peak_id ASC limit {}".format(feature_id, args.negative_rt_delta_tolerance, args.positive_rt_delta_tolerance, args.negative_scan_delta_tolerance, args.positive_scan_delta_tolerance, args.maximum_number_of_peaks_per_feature), db_conn)
     peak_correlation_df["feature_id-ms2_peak_id"] = peak_correlation_df.feature_id.astype(str) + '-' + peak_correlation_df.ms2_peak_id.astype(str)
-    ms2_peaks_df = pd.read_sql_query("select feature_id,peak_id,centroid_mz,intensity from ms2_peaks_within_window where feature_id || '-' || peak_id in {}".format(tuple(peak_correlation_df["feature_id-ms2_peak_id"])), db_conn)
+    ms2_peaks_df = pd.read_sql_query("select feature_id,peak_id,centroid_mz,intensity from deconvoluted_ions where feature_id || '-' || peak_id in {}".format(tuple(peak_correlation_df["feature_id-ms2_peak_id"])), db_conn)
     ms2_peaks_df = pd.merge(ms2_peaks_df, peak_correlation_df, left_on=['feature_id','peak_id'], right_on=['feature_id','ms2_peak_id'])
     ms2_peaks_df.drop(['peak_id','correlation','feature_id-ms2_peak_id'], inplace=True, axis=1)
     ms2_peaks_df.rename(columns={'intensity': 'ms2_peak_intensity', 'rt_distance': 'rt_delta', 'scan_distance': 'scan_delta', 'centroid_mz': 'peak_centroid_mz'}, inplace=True)
