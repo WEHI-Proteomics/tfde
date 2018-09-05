3b42cf6c4a4d4b723c91ecda76cea600524db476
Daryl Wilding-McBride <dwm@dius.com.au>
2018-09-05 21:05:12 +1000

    Updated.

--- correlate-ms2-peaks.py
+++ correlate-ms2-peaks.py
@@ -27,6 +27,8 @@ except AttributeError:
 
 @profile
 def main():
+    pd.options.mode.chained_assignment = None
+    
     parser = argparse.ArgumentParser(description='Calculate correlation between MS1 and MS2 peaks for features.')
     parser.add_argument('-db','--database_name', type=str, help='The name of the source database.', required=True)
     parser.add_argument('-cdb','--converted_database_name', type=str, help='The name of the converted database.', required=True)
@@ -129,7 +131,6 @@ def main():
             composite_mzs_min = ms2_peaks_df.loc[ms2_peak_idx].composite_mzs_min.item()
             composite_mzs_max = ms2_peaks_df.loc[ms2_peak_idx].composite_mzs_max.item()
             peak_points = ms2_feature_region_points_df[(ms2_feature_region_points_df.scaled_mz >= composite_mzs_min) & (ms2_feature_region_points_df.scaled_mz <= composite_mzs_max)]
-            peak_points.is_copy = False
             peak_points['retention_time_secs'] = peak_points.frame_id / raw_frame_ids_per_second
             ms2_centroid_scan = peakutils.centroid(peak_points.scan.astype(float), peak_points.intensity)
             ms2_centroid_rt = peakutils.centroid(peak_points.retention_time_secs.astype(float), peak_points.intensity)
