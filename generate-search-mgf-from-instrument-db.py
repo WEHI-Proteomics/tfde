527622e834e742104baec8b3150437ef724265e0
Daryl Wilding-McBride <dwm@dius.com.au>
2018-09-14 13:17:13 +1000

    Updated.

--- generate-search-mgf-from-instrument-db.py
+++ generate-search-mgf-from-instrument-db.py
@@ -517,6 +517,7 @@ if process_this_step(this_step='deconvolve_ms2_spectra', first_step=args.operati
     for idx,feature_range in enumerate(feature_ranges):
         destination_db_name = "{}-{}-{}.sqlite".format(feature_database_root, feature_range[0], feature_range[1])
         db_conn = sqlite3.connect(feature_database_name)
+        print("processing {}".format(feature_database_name))
         feature_list_df = pd.read_sql_query("select * from feature_list", db_conn)
         csv_file_name = "{}-feature-list.csv".format(feature_database_root)
         if idx == 0:
