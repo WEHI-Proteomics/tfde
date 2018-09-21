0a9b3934f7255fd0d04353c71e29d05ffd37ba45
Daryl Wilding-McBride <dwm@dius.com.au>
2018-09-21 16:09:44 +1000

    Updated.

--- feature-region-ms2-combined-sum-peak-detect.py
+++ feature-region-ms2-combined-sum-peak-detect.py
@@ -191,6 +191,7 @@ def main():
                 # drop the duplicate rows
                 frame_df.drop_duplicates(subset=('scan','scaled_mz'), inplace=True)
                 # create the frame array
+                print("frame_df length {}".len(frame_df))
                 print("scan max {}, scaled_mz max {}".format(frame_df.scan.max(), frame_df.scaled_mz.max()))
                 frame_a = np.zeros(shape=(int(frame_df.scan.max()+1),int(frame_df.scaled_mz.max()+1)), dtype=np.int32)
                 print("allocated {} bytes for the frame array".format(getsizeof(frame_a)))
