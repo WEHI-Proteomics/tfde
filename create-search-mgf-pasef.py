1a8841d6d1b439b6f924e3338752c56bb45be6e6
Daryl Wilding-McBride <dwm@dius.com.au>
2018-10-27 14:49:03 +1100

    Updated.

--- create-search-mgf-pasef.py
+++ create-search-mgf-pasef.py
@@ -53,7 +53,7 @@ try:
 
         # get all the precursors for this feature
         db_conn = sqlite3.connect(args.features_database)
-        precursors_df = pd.read_sql_query("select * from feature_isolation_matches where feature_id={}".format(feature_id), db_conn)
+        precursors_df = pd.read_sql_query("select unique(precursor_id) from feature_isolation_matches where feature_id={}".format(feature_id), db_conn)
         db_conn.close()
 
         for precursor_idx in range(len(precursors_df)):
