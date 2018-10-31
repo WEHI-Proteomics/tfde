bba004f248cd568c1d30a108cb88cf08bef07f9d
Daryl Wilding-McBride <dwm@dius.com.au>
2018-11-01 09:59:46 +1100

    Update animations/create-pngs-summed-frames.py

--- animations/create-pngs-summed-frames.py
+++ animations/create-pngs-summed-frames.py
@@ -24,29 +24,28 @@ working_folder = '/home/ubuntu/{}/{}'.format(save_folder, gif_filename)
 if not os.path.exists(working_folder):
     os.makedirs(working_folder)
 
-fig = plt.figure()
-fig.set_figheight(10)
-fig.set_figwidth(15)
-
-ax = Axes3D(fig)
-ax.elev = 20.0
-ax.azim = 250.0
-ax.dist = 10.0
-
-ax.set_xlim(left=200, right=1800)
-ax.set_ylim(bottom=900, top=0)
-ax.set_zlim(bottom=0, top=120000)
-
-plt.gca().invert_yaxis()
-plt.xlabel('m/z')
-plt.ylabel('scan')
-
 for frame_id in range(frame_lower,frame_upper):
     print("rendering frame {}".format(frame_id))
+
+    fig = plt.figure()
+    fig.set_figheight(10)
+    fig.set_figwidth(15)
+
+    ax = Axes3D(fig)
+    ax.elev = 20.0
+    ax.azim = 250.0
+    ax.dist = 10.0
+
+    ax.set_xlim(left=200, right=1800)
+    ax.set_ylim(bottom=900, top=0)
+    ax.set_zlim(bottom=0, top=120000)
+
+    plt.gca().invert_yaxis()
+    plt.xlabel('m/z')
+    plt.ylabel('scan')
+
     frame_df = frames_df[frames_df.frame_id==frame_id]
     ax.scatter(frame_df.mz, frame_df.scan, frame_df.intensity, c=np.log(frame_df.intensity), cmap='cool')
     fig.suptitle('Frame {}'.format(frame_id), fontsize=16, x=0.5, y=0.85)
     plt.savefig('{}/img{:03d}.png'.format(working_folder, frame_id), bbox_inches='tight')
-    fig.clf()
-
-plt.close()
+    plt.close()
