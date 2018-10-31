4be9c64b3199ef093c6f499bd029691b7b205679
Daryl Wilding-McBride <dwm@dius.com.au>
2018-10-31 22:49:38 +1100

    Updated.

---
+++ animations/create-animated-gif-raw-data.py
@@ -0,0 +1,47 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import matplotlib.pyplot as plt
+import os
+from PIL import Image
+import IPython.display as IPdisplay
+import glob
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[ ]:
+
+
+gif_filename = 'HeLa_20KInt'
+save_folder = 'animation'
+working_folder = '/home/ubuntu/{}/{}'.format(save_folder, gif_filename)
+
+
+# In[ ]:
+
+
+# load all the static images into a list then save as an animated gif
+gif_filepath = '{}/{}.gif'.format(working_folder, gif_filename)
+images = [Image.open(image) for image in glob.glob('{}/*.png'.format(working_folder))]
+images[0].save(gif_filepath,
+               save_all=True,
+               append_images=images[1:],
+               duration=100,
+               loop=0)
+IPdisplay.Image(url=gif_filepath)
+
+
+# In[ ]:
+
+
+gif_filepath
+
+
+# In[ ]:
+
+
+len(images)
+
