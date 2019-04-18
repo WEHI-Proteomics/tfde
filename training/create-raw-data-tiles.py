bddad308ce6a87471aa29d3647408aba86641fd4
Daryl Wilding-McBride <wilding-mcbride.d@wehi.edu.au>
2019-04-18 09:03:39 +0000

    Updated.

--- training/create-raw-data-tiles.py
+++ training/create-raw-data-tiles.py
@@ -16,18 +16,18 @@ parser.add_argument('-rtl','--rt_lower', type=int, help='Lower bound of the RT r
 parser.add_argument('-rtu','--rt_upper', type=int, help='Upper bound of the RT range.', required=True)
 args = parser.parse_args()
 
-BASE_NAME = "/home/daryl/HeLa_20KInt-rt-{}-{}".format(args.rt_lower, args.rt_upper)
+BASE_NAME = "/home/ubuntu/HeLa_20KInt-rt-{}-{}".format(args.rt_lower, args.rt_upper)
 # BASE_NAME = "/Users/darylwilding-mcbride/Downloads/HeLa_20KInt-rt-{}-{}".format(args.rt_lower, args.rt_upper)
 CONVERTED_DATABASE_NAME = '{}/HeLa_20KInt.sqlite'.format(BASE_NAME)
-ALLPEPTIDES_FILENAME = '/home/daryl/maxquant_results/txt/allPeptides.txt'
+ALLPEPTIDES_FILENAME = '/home/ubuntu/maxquant_results/txt/allPeptides.txt'
 # ALLPEPTIDES_FILENAME = '/Users/darylwilding-mcbride/Downloads/maxquant_results/txt/allPeptides.txt'
 
 # TILE_BASE = '/Users/darylwilding-mcbride/Downloads/yolo-train'
-TILE_BASE = '/home/daryl/yolo-train-rt-{}-{}'.format(args.rt_lower, args.rt_upper)
+TILE_BASE = '/home/ubuntu/yolo-train-rt-{}-{}'.format(args.rt_lower, args.rt_upper)
 PRE_ASSIGNED_FILES_DIR = '{}/pre-assigned'.format(TILE_BASE)
 OVERLAY_FILES_DIR = '{}/overlay'.format(TILE_BASE)
 
-if not input("This will erase the overlay and pre-assigned directories in {}. Are you sure? (y/n): ".format(TILE_BASE)).lower().strip()[:1] == "y": sys.exit(1)
+# if not input("This will erase the overlay and pre-assigned directories in {}. Are you sure? (y/n): ".format(TILE_BASE)).lower().strip()[:1] == "y": sys.exit(1)
 
 db_conn = sqlite3.connect(CONVERTED_DATABASE_NAME)
 ms1_frame_properties_df = pd.read_sql_query("select frame_id,retention_time_secs from frame_properties where retention_time_secs >= {} and retention_time_secs <= {} and collision_energy == {}".format(args.rt_lower, args.rt_upper, MS1_CE), db_conn)
@@ -276,4 +276,5 @@ def render_tile_for_frame(frame_r):
 # ray.get([render_tile_for_frame.remote(frame_r) for frame_r in zip(ms1_frame_properties_df.iloc[:1].frame_id, ms1_frame_properties_df.iloc[:1].retention_time_secs)])
 ray.get([render_tile_for_frame.remote(frame_r) for frame_r in zip(ms1_frame_properties_df.frame_id, ms1_frame_properties_df.retention_time_secs)])
 
+print("shutting down ray")
 ray.shutdown()
