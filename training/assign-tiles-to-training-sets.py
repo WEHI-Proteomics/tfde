6f72ea53b79d8633a9eea13b2e4b22a49c3e52bc
Daryl Wilding-McBride <dwm@dius.com.au>
2019-04-28 12:46:50 +1000

    Introduced bands.

--- training/assign-tiles-to-training-sets.py
+++ training/assign-tiles-to-training-sets.py
@@ -11,6 +11,7 @@ import argparse
 MS1_CE = 10
 
 SET_GAP = 10  # number of frames between training sets to avoid features appearing in more than one set
+NUMBER_OF_BANDS = 4  # periods over the run from which mini sets will be taken
 
 TRAINING_SET_PROPORTION = 0.8
 VALIDATION_SET_PROPORTION = 0.1
@@ -45,33 +46,55 @@ for file in glob.glob("{}/*.png".format(PRE_ASSIGNED_FILES_DIR)):
 fn_df = pd.DataFrame(filenames, columns=['name'])
 
 # allocate the training, validation, and test sets from separate periods of RT, as frames are a time series - with an N-frame gap between sets
-def train_validate_test_split_v4(df, train_percent=TRAINING_SET_PROPORTION, validate_percent=VALIDATION_SET_PROPORTION, seed=None):
+def train_validate_test_split_v4(tile_filenames_df, ms1_frames_df, train_percent=TRAINING_SET_PROPORTION, validate_percent=VALIDATION_SET_PROPORTION, seed=None):
     test_percent = 1.0 - (train_percent + validate_percent)
 
-    # split the ms1 frame ids into three sections according to their proportions
-    split_1 = int(train_percent*len(ms1_frame_properties_df))
-    split_1_plus_gap = split_1 + SET_GAP
-    split_2 = int((train_percent+validate_percent)*len(ms1_frame_properties_df))
-    split_2_plus_gap = split_2 + SET_GAP
-    train_ids_df, gap_1_df, valid_ids_df, gap_2_df, test_ids_df = np.split(ms1_frame_properties_df, [split_1, split_1_plus_gap, split_2, split_2_plus_gap])
-    print("training set: {:.1f} to {:.1f} secs ({} frames)".format(train_ids_df.retention_time_secs.min(), train_ids_df.retention_time_secs.max(), len(train_ids_df)))
-    print("validation set: {:.1f} to {:.1f} secs ({} frames)".format(valid_ids_df.retention_time_secs.min(), valid_ids_df.retention_time_secs.max(), len(valid_ids_df)))
-    print("test set: {:.1f} to {:.1f} secs ({} frames)".format(test_ids_df.retention_time_secs.min(), test_ids_df.retention_time_secs.max(), len(test_ids_df)))
-    
-    train_terms = ['frame-' + str(s) for s in train_ids_df.frame_id]
-    train_df = df[df['name'].str.contains('|'.join(train_terms))].copy()
+    # divide the ms1 frames into equal-size bands
+    bands = np.array_split(ms1_frames_df, NUMBER_OF_BANDS)
 
-    valid_terms = ['frame-' + str(s) for s in valid_ids_df.frame_id]
-    valid_df = df[df['name'].str.contains('|'.join(valid_terms))].copy()
+    train_terms = []
+    valid_terms = []
+    test_terms = []
 
-    test_terms = ['frame-' + str(s) for s in test_ids_df.frame_id]
-    test_df = df[df['name'].str.contains('|'.join(test_terms))].copy()
+    for band_idx,band_df in enumerate(bands):
+
+        band_length = len(band_df)
+        train_length = int(train_percent * band_length)
+        valid_length = int(valid_percent * band_length)
+        test_length = int(test_percent * band_length)
+
+        train_start = 0
+        train_stop = train_start + train_length - SET_GAP
+
+        valid_start = train_start + train_length
+        valid_stop = valid_start + valid_length - SET_GAP
+
+        test_start = valid_start + valid_length
+        test_stop = test_start + test_length - SET_GAP
+
+        # split the band into three sections according to their proportions
+        train_ids_df, gap_1_df, valid_ids_df, gap_2_df, test_ids_df, gap_3_df = np.split(band_df, [train_stop, valid_start, valid_stop, test_start, test_stop])
+
+        print("band {}".format(band_idx))
+        print("training set: {:.1f} to {:.1f} secs ({} frames)".format(train_ids_df.retention_time_secs.min(), train_ids_df.retention_time_secs.max(), len(train_ids_df)))
+        print("validation set: {:.1f} to {:.1f} secs ({} frames)".format(valid_ids_df.retention_time_secs.min(), valid_ids_df.retention_time_secs.max(), len(valid_ids_df)))
+        print("test set: {:.1f} to {:.1f} secs ({} frames)".format(test_ids_df.retention_time_secs.min(), test_ids_df.retention_time_secs.max(), len(test_ids_df)))
+        print('\n')
+
+        train_terms += ['frame-' + str(s) for s in train_ids_df.frame_id]
+        valid_terms += ['frame-' + str(s) for s in valid_ids_df.frame_id]
+        test_terms += ['frame-' + str(s) for s in test_ids_df.frame_id]
+
+    # collate the filenames for these frames
+    train_df = tile_filenames_df[tile_filenames_df['name'].str.contains('|'.join(train_terms))].copy()
+    valid_df = tile_filenames_df[tile_filenames_df['name'].str.contains('|'.join(valid_terms))].copy()
+    test_df = tile_filenames_df[tile_filenames_df['name'].str.contains('|'.join(test_terms))].copy()
 
     return train_df, valid_df, test_df
 
-train_df,validate_df,test_df = train_validate_test_split_v4(fn_df)
+train_df,validate_df,test_df = train_validate_test_split_v4(fn_df, ms1_frame_properties_df)
 
-print("number of images in train set: {}, validation set: {}, test set {}".format(len(train_df),len(validate_df),len(test_df)))
+print("number of tiles in train set: {}, validation set: {}, test set {}".format(len(train_df),len(validate_df),len(test_df)))
 
 data_dirs = ['train', 'validation', 'test']
 data_dfs = [train_df, validate_df, test_df]
@@ -88,13 +111,13 @@ os.makedirs(LOCAL_DATA_FILES_DIR)
 
 for idx,dd in enumerate(data_dirs):
     print("processing {}".format(dd))
-    
+
     # initialise the directory for each set
     data_dir = "{}/{}".format(TILE_BASE, dd)
     if os.path.exists(data_dir):
         shutil.rmtree(data_dir)
     os.makedirs(data_dir)
-    
+
     # create the prefix column
     data_dfs[idx]['text_entry'] = '{}/{}/'.format(DESTINATION_DATASET_BASE, dd) + data_dfs[idx].name + '.png'
 
