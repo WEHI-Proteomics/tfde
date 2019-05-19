c28b44798c24b3786af2e47419d9bbe6872da4da
Daryl Wilding-McBride <wilding-mcbride.d@wehi.edu.au>
2019-05-19 01:56:51 +0000

    Updated.

--- training/make-list-of-files-for-bulk-prediction.py
+++ training/make-list-of-files-for-bulk-prediction.py
@@ -1,10 +1,10 @@
 import glob,os
 
-TEST_DIR = '~/yolo-train/test/'
+TEST_DIR = './yolo-train-rt-1000-4200-15-may/test/'
 TARGET_DIR = 'data/peptides/test'
 TILE_ID = 33
-FILE_LIST_FILENAME = '~/test-files.txt'
+FILE_LIST_FILENAME = './test-files.txt'
 
 with open(FILE_LIST_FILENAME, 'w') as f:
-    for file in sorted(glob.glob("{}/frame-*-tile-{}-mz-*.png".format(PREASSIGNED_DIR, TILE_ID))):
+    for file in sorted(glob.glob("{}/frame-*-tile-{}-mz-*.png".format(TEST_DIR, TILE_ID))):
         f.write('{}/{}\n'.format(TARGET_DIR, os.path.basename(file)))
