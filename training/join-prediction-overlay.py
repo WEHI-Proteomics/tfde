229b2eeb5601d68d91485fe9d9c28138b5a68e65
Daryl Wilding-McBride <wilding-mcbride.d@wehi.edu.au>
2019-05-19 10:46:29 +0000

    Updated.

--- training/join-prediction-overlay.py
+++ training/join-prediction-overlay.py
@@ -1,16 +1,18 @@
 import glob,os
 
-PREDICTION_DIR = '~/Downloads/predictions'
+PREDICTION_DIR = './darknet/result_img'
 BASE_DIR = './yolo-train-rt-1000-4200-15-may'
 TEST_DIR = '{}/test'.format(BASE_DIR)
 OVERLAY_DIR = '{}/overlay'.format(BASE_DIR)
-COMBINED_DIR = '~/Downloads/combined'
+COMBINED_DIR = './combined'
+TILE_ID = 33
 
-file_list = sorted(glob.glob("{}/frame-*-tile-{}-mz-*.png".format(PREDICTION_DIR, TILE_ID)))
+file_list = sorted(glob.glob("{}/prediction-frame-*-tile-{}-mz-*.jpg".format(PREDICTION_DIR, TILE_ID)))
 
 for prediction_file in file_list:
-    basename = os.path.basename(prediction_file)
-    overlay_name = "{}/{}".format(OVERLAY_DIR, basename)
-    combined_name = "{}/combined-{}".format(COMBINED_DIR, basename)
+    basename = os.path.splitext(os.path.basename(prediction_file).replace('prediction-', ''))[0]
+    print("processing {}".format(basename))
+    overlay_name = "{}/{}.png".format(OVERLAY_DIR, basename)
+    combined_name = "{}/combined-{}.png".format(COMBINED_DIR, basename)
     cmd = "convert {} {} +append -background darkgrey -splice 10x0+910+0 {}".format(prediction_file, overlay_name, combined_name)
     os.system(cmd)
