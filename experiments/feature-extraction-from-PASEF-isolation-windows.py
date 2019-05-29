966e84880551a7010ad65a254c60f6646169380d
Daryl Wilding-McBride <dwm@dius.com.au>
2019-05-29 16:26:53 +1000

    Updated.

--- experiments/feature-extraction-from-PASEF-isolation-windows.py
+++ experiments/feature-extraction-from-PASEF-isolation-windows.py
@@ -342,12 +342,15 @@ def remove_ms1_duplicates(ms1_features_df):
         cond_1 = (scratch_df.monoisotopic_mz >= mz_lower) & (scratch_df.monoisotopic_mz <= mz_upper) & (scratch_df.scan_apex >= scan_lower) & (scratch_df.scan_apex <= scan_upper) & (scratch_df.rt_apex >= rt_lower) & (scratch_df.rt_apex <= rt_upper)
         matching_rows = scratch_df.loc[cond_1, :]
         scratch_df.drop(matching_rows.index, inplace=True)
-        # of those, find the most intense
-        cond_2 = matching_rows.intensity.idxmax()
-        most_intense_row = matching_rows.loc[cond_2, :].copy()
-        most_intense_row['duplicates'] = len(matching_rows)
-        # add it to the list
-        ms1_features_l.append(tuple(most_intense_row))
+        if len(matching_rows) == 1:
+            ms1_features_l.append(tuple(matching_rows.iloc[0]))
+        else:
+            # of those, find the most intense
+            cond_2 = matching_rows.intensity.idxmax()
+            most_intense_row = matching_rows.loc[cond_2, :].copy()
+            most_intense_row['duplicates'] = len(matching_rows)
+            # add it to the list
+            ms1_features_l.append(tuple(most_intense_row))
 
         print("{} matches, {} remaining".format(len(matching_rows), len(scratch_df)))
 
