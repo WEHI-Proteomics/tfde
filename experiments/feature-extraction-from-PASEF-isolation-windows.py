afa4470e41d246fb697db33d71d150677fd498f9
Daryl Wilding-McBride <dwm@dius.com.au>
2019-05-30 14:45:14 +1000

    Updated.

--- experiments/feature-extraction-from-PASEF-isolation-windows.py
+++ experiments/feature-extraction-from-PASEF-isolation-windows.py
@@ -41,7 +41,7 @@ if not ray.is_initialized():
     if args.cluster_mode:
         ray.init(redis_address="localhost:6379")
     else:
-        ray.init(object_store_memory=50000000000)
+        ray.init(object_store_memory=100000000000)
 
 start_run = time.time()
 
