093a4faaf844a95c3913aeaf64884d6e4d7c40e0
Daryl Wilding-McBride <dwm@dius.com.au>
2019-06-19 14:52:11 +1000

    Adjusted error tolerance in the ms2 deconvolution.

--- experiments/feature-extraction-from-PASEF-isolation-windows.py
+++ experiments/feature-extraction-from-PASEF-isolation-windows.py
@@ -410,7 +410,7 @@ def deconvolute_ms2_peaks_for_feature(feature_id, ms2_frame_id, binned_ms2_df):
     print("{} ms2 peaks prior to deconvolution".format(len(ms2_peaks_df)))
 
     # deconvolute the peaks
-    ms2_deconvoluted_peaks, _ = deconvolute_peaks(ms2_peaks_l, averagine=averagine.peptide, charge_range=(1,5), scorer=scoring.MSDeconVFitter(minimum_score=8, mass_error_tolerance=0.1), truncate_after=0.6)
+    ms2_deconvoluted_peaks, _ = deconvolute_peaks(ms2_peaks_l, averagine=averagine.peptide, charge_range=(1,5), scorer=scoring.MSDeconVFitter(minimum_score=8, mass_error_tolerance=0.1), error_tolerance=4e-5, truncate_after=0.6)
 
     print("{} ms2 peaks after deconvolution".format(len(ms2_deconvoluted_peaks)))
 
