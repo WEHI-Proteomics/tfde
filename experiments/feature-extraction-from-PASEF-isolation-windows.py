fd62445e34fe689bed79fb8cc5302a9f0ab6a26b
Daryl Wilding-McBride <dwm@dius.com.au>
2019-06-24 14:11:07 +1000

    Updated.

--- experiments/feature-extraction-from-PASEF-isolation-windows.py
+++ experiments/feature-extraction-from-PASEF-isolation-windows.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import sys
 import peakutils
-from ms_deisotope import deconvolute_peaks, averagine, scoring, deconvolution.peak_retention_strategy
+from ms_deisotope import deconvolute_peaks, averagine, scoring, deconvolution
 from pyteomics import mgf
 import os.path
 import argparse
