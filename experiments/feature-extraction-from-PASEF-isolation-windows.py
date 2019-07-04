fd6aaab4409308e1336aee4c7084549fde79b0f2
Daryl Wilding-McBride <dwm@dius.com.au>
2019-07-04 15:11:14 +1000

    Intensity descent in numpy.

--- experiments/feature-extraction-from-PASEF-isolation-windows.py
+++ experiments/feature-extraction-from-PASEF-isolation-windows.py
@@ -357,29 +357,29 @@ def find_features(group_number, group_df):
             binned_ms1_l.append((mz_cent,summed_intensity))
 
     binned_ms1_df = pd.DataFrame(binned_ms1_l, columns=['mz_centroid','summed_intensity'])
-    raw_scratch_df = binned_ms1_df.copy() # take a copy because we're going to delete stuff
     if args.test_mode:
         binned_ms1_df.to_csv('./ms1-peaks-group-{}-before-intensity-descent.csv'.format(group_number), index=False, header=True)
 
+    # do intensity descent to find the peaks
+    mzs = binned_ms1_df.mz_centroid.to_numpy()
+    intensities = binned_ms1_df.summed_intensity.to_numpy()
     ms1_peaks_l = []
-    while len(raw_scratch_df) > 0:
+    while len(mzs) > 0:
         # find the most intense point
-        peak_df = raw_scratch_df.loc[raw_scratch_df.summed_intensity.idxmax()]
-        peak_mz = peak_df.mz_centroid
+        max_intensity_index = np.argmax(intensities)
+        peak_mz = mzs[max_intensity_index]
         peak_mz_lower = peak_mz - args.ms1_peak_delta
         peak_mz_upper = peak_mz + args.ms1_peak_delta
 
         # get all the raw points within this m/z region
-        peak_rows = np.where((raw_scratch_df.mz_centroid >= peak_mz_lower) & (raw_scratch_df.mz_centroid <= peak_mz_upper))
-        peak_raw_points_df = raw_scratch_df.iloc[peak_rows]
-        if len(peak_raw_points_df) > 0:
-            mz_cent = mz_centroid(peak_raw_points_df.summed_intensity.to_numpy(), peak_raw_points_df.mz_centroid.to_numpy())
-            summed_intensity = peak_raw_points_df.summed_intensity.sum()
-            ms1_peaks_l.append((mz_cent, summed_intensity))
-
+        peak_indexes = np.where((mzs >= peak_mz_lower) & (mzs <= peak_mz_upper))
+        if len(peak_indexes) > 0:
+            mz_cent = mz_centroid(intensities[peak_indexes], mzs[peak_indexes])
+            summed_intensity = intensities[peak_indexes].sum()
+            ms1_peaks_l.append(simple_peak(mz=mz_cent, intensity=summed_intensity))
             # remove the raw points assigned to this peak
-            raw_scratch_df.drop(labels=raw_scratch_df.index[peak_rows], inplace=True)
-
+            intensities = np.delete(intensities, peak_indexes)
+            mzs = np.delete(mzs, peak_indexes)
 
     ms1_peaks_df = pd.DataFrame(ms1_peaks_l, columns=['mz','intensity'])
     if args.test_mode:
