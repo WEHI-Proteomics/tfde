b1cd90871404e69f212df523619c533b44b60d71
Daryl Wilding-McBride <dwm@dius.com.au>
2019-08-06 07:08:57 +1000

    No longer in the background.

--- pda/bulk-convert-raw-databases.py
+++ pda/bulk-convert-raw-databases.py
@@ -15,5 +15,5 @@ for file in glob.glob("{}/*.d".format(RAW_DIR)):
     db_name = os.path.basename(file)
     convert_dir_name = db_name.split('_Slot')[0]
     os.makedirs('{}/{}'.format(CONVERTED_DIR, convert_dir_name))
-    cmd = "nohup python -u ~/otf-peak-detect/original-pipeline/convert-instrument-db.py -sdb {}/{} -ddb {}/{}/{}-converted.sqlite -bs 2000 > {}/{}.log 2>&1 &".format(RAW_DIR, db_name, CONVERTED_DIR, convert_dir_name, convert_dir_name, BASE_DIR, convert_dir_name)
+    cmd = "python -u ~/otf-peak-detect/original-pipeline/convert-instrument-db.py -sdb {}/{} -ddb {}/{}/{}-converted.sqlite -bs 2000 > {}/{}.log 2>&1".format(RAW_DIR, db_name, CONVERTED_DIR, convert_dir_name, convert_dir_name, BASE_DIR, convert_dir_name)
     os.system(cmd)
