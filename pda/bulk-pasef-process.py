741b8471ba82e5c1b29fb1e28b22f3f74035d384
Daryl Wilding-McBride <dwm@dius.com.au>
2019-08-06 16:24:46 +1000

    Debug.

--- pda/bulk-pasef-process.py
+++ pda/bulk-pasef-process.py
@@ -12,5 +12,5 @@ for raw_db_file in glob.glob("{}/*.d".format(RAW_DIR)):
     base_processing_dir = CONVERTED_DIR
     print("processing {}".format(db_name))
     cmd = "python -u ~/otf-peak-detect/pda/pasef-process.py -rdb {} -bpd {} -pn {} -ini {} -os linux > {}/{}.log 2>&1".format(raw_db_file, base_processing_dir, db_name, INI_FILE, base_processing_dir, db_name)
-    print(cmd)
-    # os.system(cmd)
+    # print(cmd)
+    os.system(cmd)
