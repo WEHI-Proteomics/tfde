6d50a7470348170ae8d45cc3dc11f4df5a2eb43b
Daryl Wilding-McBride <dwm@dius.com.au>
2019-08-07 09:02:40 +1000

    Added timing info.

--- pda/bulk-convert-raw-databases.py
+++ pda/bulk-convert-raw-databases.py
@@ -1,10 +1,13 @@
 import glob
 import os
 import shutil
+import time
 
 BASE_DIR = '/home/ubuntu/190719_Hela_Ecoli/190719_Hela_Ecoli_1to1'
 RAW_DIR = '{}/raw'.format(BASE_DIR)
 CONVERTED_DIR = '{}/converted'.format(BASE_DIR)
+
+start_run = time.time()
 
 if os.path.exists(CONVERTED_DIR):
     shutil.rmtree(CONVERTED_DIR)
@@ -18,3 +21,6 @@ for file in glob.glob("{}/*.d".format(RAW_DIR)):
     os.makedirs('{}/{}'.format(CONVERTED_DIR, convert_dir_name))
     cmd = "python -u ~/otf-peak-detect/original-pipeline/convert-instrument-db.py -sdb {}/{} -ddb {}/{}/{}-converted.sqlite -bs 2000 > {}/{}-convert.log 2>&1".format(RAW_DIR, db_name, CONVERTED_DIR, convert_dir_name, convert_dir_name, BASE_DIR, convert_dir_name)
     os.system(cmd)
+
+stop_run = time.time()
+print("total running time (bulk-convert-raw-databases): {} seconds".format(round(stop_run-start_run,1)))
--- pda/bulk-pasef-process.py
+++ pda/bulk-pasef-process.py
@@ -1,11 +1,14 @@
 import glob
 import os
 import shutil
+import time
 
 BASE_DIR = '/home/ubuntu/190719_Hela_Ecoli/190719_Hela_Ecoli_1to1'
 RAW_DIR = '{}/raw'.format(BASE_DIR)
 CONVERTED_DIR = '{}/converted'.format(BASE_DIR)
 INI_FILE = '/home/ubuntu/otf-peak-detect/pda/pasef-process-short-gradient.ini'
+
+start_run = time.time()
 
 for raw_db_file in glob.glob("{}/*.d".format(RAW_DIR)):
     db_name = os.path.basename(raw_db_file).split('_Slot')[0]
@@ -14,3 +17,6 @@ for raw_db_file in glob.glob("{}/*.d".format(RAW_DIR)):
     cmd = "python -u ~/otf-peak-detect/pda/pasef-process.py -rdb {} -bpd {} -pn {} -ini {} -os linux > {}/{}-processing.log 2>&1".format(raw_db_file, base_processing_dir, db_name, INI_FILE, BASE_DIR, db_name)
     # print(cmd)
     os.system(cmd)
+
+stop_run = time.time()
+print("total running time (bulk-pasef-process): {} seconds".format(round(stop_run-start_run,1)))
