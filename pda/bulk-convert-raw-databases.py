8782fb2836f7263e5471968d016b11c7fc062bc6
Daryl Wilding-McBride <dwm@dius.com.au>
2019-08-07 09:51:31 +1000

    Added shutdown.

--- pda/bulk-convert-raw-databases.py
+++ pda/bulk-convert-raw-databases.py
@@ -2,6 +2,15 @@ import glob
 import os
 import shutil
 import time
+import argparse
+
+def run_process(process):
+    print("Executing: {}".format(process))
+    os.system(process)
+
+parser = argparse.ArgumentParser(description='Convert the raw databases to processing databases.')
+parser.add_argument('-shutdown','--shutdown', action='store_true', help='Shut down the machine when complete.')
+args = parser.parse_args()
 
 BASE_DIR = '/home/ubuntu/190719_Hela_Ecoli/190719_Hela_Ecoli_1to1'
 RAW_DIR = '{}/raw'.format(BASE_DIR)
@@ -20,7 +29,10 @@ for file in glob.glob("{}/*.d".format(RAW_DIR)):
     print("processing {}".format(convert_dir_name))
     os.makedirs('{}/{}'.format(CONVERTED_DIR, convert_dir_name))
     cmd = "python -u ~/otf-peak-detect/original-pipeline/convert-instrument-db.py -sdb {}/{} -ddb {}/{}/{}-converted.sqlite -bs 2000 > {}/{}-convert.log 2>&1".format(RAW_DIR, db_name, CONVERTED_DIR, convert_dir_name, convert_dir_name, BASE_DIR, convert_dir_name)
-    os.system(cmd)
+    run_process(cmd)
 
 stop_run = time.time()
 print("total running time (bulk-convert-raw-databases): {} seconds".format(round(stop_run-start_run,1)))
+
+if args.shutdown:
+    run_process("sudo shutdown -P +5") # shutdown the instance in 5 minutes from now
--- pda/bulk-pasef-process.py
+++ pda/bulk-pasef-process.py
@@ -2,6 +2,15 @@ import glob
 import os
 import shutil
 import time
+import argparse
+
+def run_process(process):
+    print("Executing: {}".format(process))
+    os.system(process)
+
+parser = argparse.ArgumentParser(description='Manage the ms1 and ms2 processing, and generate the MGF.')
+parser.add_argument('-shutdown','--shutdown', action='store_true', help='Shut down the machine when complete.')
+args = parser.parse_args()
 
 BASE_DIR = '/home/ubuntu/190719_Hela_Ecoli/190719_Hela_Ecoli_1to1'
 RAW_DIR = '{}/raw'.format(BASE_DIR)
@@ -15,8 +24,10 @@ for raw_db_file in glob.glob("{}/*.d".format(RAW_DIR)):
     base_processing_dir = CONVERTED_DIR
     print("processing {}".format(db_name))
     cmd = "python -u ~/otf-peak-detect/pda/pasef-process.py -rdb {} -bpd {} -pn {} -ini {} -os linux > {}/{}-processing.log 2>&1".format(raw_db_file, base_processing_dir, db_name, INI_FILE, BASE_DIR, db_name)
-    # print(cmd)
-    os.system(cmd)
+    run_process(cmd)
 
 stop_run = time.time()
 print("total running time (bulk-pasef-process): {} seconds".format(round(stop_run-start_run,1)))
+
+if args.shutdown:
+    run_process("sudo shutdown -P +5") # shutdown the instance in 5 minutes from now
