96de3a03391e9326a83bdad88c8a37728c77d6b8
Daryl Wilding-McBride <dwm@dius.com.au>
2019-09-02 15:54:34 +1000

    Updated for new directory structure.

--- pda/bulk-comet-search.py
+++ pda/bulk-comet-search.py
@@ -9,13 +9,28 @@ def run_process(process):
     os.system(process)
 
 parser = argparse.ArgumentParser(description='Find all the MGFs and run a Comet search on them.')
+parser.add_argument('-eb','--experiment_base_dir', type=str, default='./experiments', help='Path to the experiments directory.', required=False)
+parser.add_argument('-en','--experiment_name', type=str, help='Name of the experiment.', required=True)
 parser.add_argument('-recal','--recalibration_mode', action='store_true', help='Use a tighter tolerance on the search.')
 parser.add_argument('-shutdown','--shutdown', action='store_true', help='Shut down the machine when complete.')
 args = parser.parse_args()
 
-RUN_NAME = "190719_Hela_Ecoli"
-BASE_DIR = '/home/ubuntu/{}'.format(RUN_NAME)
-COMET_OUTPUT_DIR = "{}/comet-output".format(BASE_DIR)
+# check the experiment directory exists
+EXPERIMENT_DIR = "{}/{}".format(args.experiment_base_dir, args.experiment_name)
+if not os.path.exists(EXPERIMENT_DIR):
+    print("The experiment directory is required but doesn't exist: {}".format(EXPERIMENT_DIR))
+    sys.exit(1)
+
+COMET_OUTPUT_DIR = "{}/comet-output".format(EXPERIMENT_DIR)
+if not os.path.exists(COMET_OUTPUT_DIR):
+    os.makedirs(COMET_OUTPUT_DIR)
+    print("The comet output directory was created: {}".format(COMET_OUTPUT_DIR))
+
+MGF_DIR = "{}/mgfs".format(EXPERIMENT_DIR)
+if not os.path.exists(MGF_DIR):
+    print("The MGF directory is required but does not exist: {}".format(MGF_DIR))
+    sys.exit(1)
+
 if args.recalibration_mode:
     COMET_PARAMETER_FILE = './otf-peak-detect/comet/TimsTOF-recalibration.params'
 else:
@@ -24,11 +39,11 @@ else:
 start_run = time.time()
 
 # process all the MGFs in the base directory
-for file in glob.glob('{}/**/*.mgf'.format(BASE_DIR), recursive=True):
+for file in glob.glob('{}/*.mgf'.format(MGF_DIR)):
     mgf_name = os.path.basename(file)
-    db_name = mgf_name.split('-search')[0]
-    print("processing {}".format(db_name))
-    cmd = "./crux-3.2.Linux.x86_64/bin/crux comet --parameter-file {} --output-dir {} --fileroot \"{}\" {} ./otf-peak-detect/fasta/uniprot-proteome-human-Ecoli.fasta".format(COMET_PARAMETER_FILE, COMET_OUTPUT_DIR, db_name, file)
+    run_name = mgf_name.split('-search')[0]
+    print("processing {}".format(run_name))
+    cmd = "./crux-3.2.Linux.x86_64/bin/crux comet --parameter-file {} --output-dir {} --fileroot \"{}\" {} ./otf-peak-detect/fasta/uniprot-proteome-human-Ecoli.fasta".format(COMET_PARAMETER_FILE, COMET_OUTPUT_DIR, run_name, file)
     run_process(cmd)
 
 stop_run = time.time()
