6f20fba03aadfca51175c13c5a994ce4f78ae5d3
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2020-01-02 11:42:17 +1100

    Update create-3d-pngs-for-animation.py

--- animations/create-3d-pngs-for-animation.py
+++ animations/create-3d-pngs-for-animation.py
@@ -9,34 +9,33 @@ import os
 import shutil
 
 
-CONVERTED_DATABASE_NAME = '/Users/darylwilding-mcbride/Downloads/experiments/190719_Hela_Ecoli/converted-databases/190719_Hela_Ecoli_1to3_06-converted.sqlite'
+CONVERTED_DATABASE_NAME = '/Users/darylwilding-mcbride/Downloads/experiments/dwm-test/converted-databases/exp-dwm-test-run-190719_Hela_Ecoli_1to1_01-converted.sqlite'
 RT_LOWER = 200
 RT_UPPER = 400
-MS1_COLLISION_ENERGY = 10
+
+# frame types for PASEF mode
+FRAME_TYPE_MS1 = 0
+FRAME_TYPE_MS2 = 8
 
 db_conn = sqlite3.connect(CONVERTED_DATABASE_NAME)
-ms1_frame_properties_df = pd.read_sql_query("select frame_id,retention_time_secs from frame_properties where retention_time_secs >= {} and retention_time_secs <= {} and collision_energy == {} order by retention_time_secs".format(RT_LOWER, RT_UPPER, MS1_COLLISION_ENERGY), db_conn)
-ms1_frame_ids = tuple(ms1_frame_properties_df.frame_id)
+frames_df = pd.read_sql_query("select frame_id,mz,scan,intensity,retention_time_secs from frames where frame_type == {} and retention_time_secs >= {} and retention_time_secs <= {} order by frame_id ASC, scan ASC, mz ASC".format(FRAME_TYPE_MS1, RT_LOWER, RT_UPPER), db_conn)
 db_conn.close()
 
-db_conn = sqlite3.connect(CONVERTED_DATABASE_NAME)
-frames_df = pd.read_sql_query("select frame_id,mz,scan,intensity from frames where frame_id in {} order by frame_id ASC, scan ASC, mz ASC".format(ms1_frame_ids), db_conn)
-db_conn.close()
+print("loaded {} points from {}".format(len(frames_df), CONVERTED_DATABASE_NAME))
 
 # set a filename, run the logistic model, and create the plot
-gif_filename = '190719_Hela_Ecoli_1to3_06'
+gif_filename = '190719_Hela_Ecoli_1to1_01'
 save_folder = '3d'
-working_folder = '/Users/darylwilding-mcbride/Downloads/experiments/190719_Hela_Ecoli/tiles/{}/{}'.format(save_folder, gif_filename)
+working_folder = '/Users/darylwilding-mcbride/Downloads/experiments/dwm-test/tiles/{}/{}'.format(save_folder, gif_filename)
 if os.path.exists(working_folder):
     shutil.rmtree(working_folder)
 os.makedirs(working_folder)
 
 azimuth = 230
+frame_counter = 0
 
-for frame_idx,frame_id in enumerate(ms1_frame_ids):
-    frame_df = frames_df[frames_df.frame_id==frame_id]
-
-    print("rendering frame {} of {}".format(frame_idx+1, len(ms1_frame_ids)))
+for frame_id,frame_df in frames_df.groupby('frame_id'):
+    print("rendering frame {}".format(frame_id))
 
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
@@ -57,6 +56,8 @@ for frame_idx,frame_id in enumerate(ms1_frame_ids):
     plt.ylabel('scan')
 
     ax.scatter(frame_df.mz, frame_df.scan, frame_df.intensity, s=2**2, c=np.log(frame_df.intensity), cmap=plt.get_cmap('cool'))
-    fig.suptitle('Frame {}'.format(frame_id), fontsize=16, x=0.5, y=0.85)
-    plt.savefig('{}/img-{:04d}.png'.format(working_folder, frame_idx), bbox_inches='tight')
+    fig.suptitle('frame {}, retention time (secs) {}'.format(frame_id, round(frame_df.iloc[0].retention_time_secs, 1)), fontsize=16, x=0.5, y=0.85)
+    plt.savefig('{}/img-{:04d}.png'.format(working_folder, frame_counter), bbox_inches='tight')
     plt.close()
+
+    frame_counter += 1
