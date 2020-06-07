811b447d47ac7007ca12cf3b3d08854cf8db0d9d
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2020-06-07 17:15:52 +1000

    Update tile-converter.py

--- yolo-feature-detection/jetson/tile-converter.py
+++ yolo-feature-detection/jetson/tile-converter.py
@@ -20,6 +20,7 @@ MZ_PER_TILE = 18.0
 TILES_PER_FRAME = int((MZ_MAX - MZ_MIN) / MZ_PER_TILE) + 1
 MIN_TILE_IDX = 0
 MAX_TILE_IDX = TILES_PER_FRAME-1
+TILE_SCALE = MZ_PER_TILE * PIXELS_X
 
 MINIMUM_PIXEL_INTENSITY = 1
 MAXIMUM_PIXEL_INTENSITY = 1000
@@ -27,58 +28,10 @@ MAXIMUM_PIXEL_INTENSITY = 1000
 def tile_pixel_x_from_mz(mz):
     assert (mz >= MZ_MIN) and (mz <= MZ_MAX), "m/z not in range"
 
-    tile_id = int((mz - MZ_MIN) / MZ_PER_TILE)
-    pixel_x = int(((mz - MZ_MIN) % MZ_PER_TILE) / MZ_PER_TILE * PIXELS_X)
+    mz_adj = mz - MZ_MIN
+    tile_id = int(mz_adj / MZ_PER_TILE)
+    pixel_x = int((mz_adj - (tile_id * MZ_PER_TILE)) / TILE_SCALE)
     return (tile_id, pixel_x)
-
-def interpolate_pixels(tile_im_array):
-    z = np.array([0,0,0])  # empty pixel check
-    for x in range(4, PIXELS_X-4):
-        for y in range(4, PIXELS_Y-4):
-            c = tile_im_array[y,x]
-            if (not c.any()) and tile_im_array[y-4:y+4,x-1:x+1].any():  # this is a zero pixel and there is a non-zero pixel in this region
-                # build the list of this pixel's neighbours to average
-                n = []
-                n.append(tile_im_array[y+1,x-1])
-                n.append(tile_im_array[y+1,x])
-                n.append(tile_im_array[y+1,x+1])
-
-                n.append(tile_im_array[y-1,x-1])
-                n.append(tile_im_array[y-1,x])
-                n.append(tile_im_array[y-1,x+1])
-
-                n.append(tile_im_array[y+2,x-1])
-                n.append(tile_im_array[y+2,x])
-                n.append(tile_im_array[y+2,x+1])
-
-                n.append(tile_im_array[y-2,x-1])
-                n.append(tile_im_array[y-2,x])
-                n.append(tile_im_array[y-2,x+1])
-
-                n.append(tile_im_array[y+3,x-1])
-                n.append(tile_im_array[y+3,x])
-                n.append(tile_im_array[y+3,x+1])
-
-                n.append(tile_im_array[y-3,x-1])
-                n.append(tile_im_array[y-3,x])
-                n.append(tile_im_array[y-3,x+1])
-
-                n.append(tile_im_array[y+4,x-1])
-                n.append(tile_im_array[y+4,x])
-                n.append(tile_im_array[y+4,x+1])
-
-                n.append(tile_im_array[y-4,x-1])
-                n.append(tile_im_array[y-4,x])
-                n.append(tile_im_array[y-4,x+1])
-
-                n.append(tile_im_array[y,x-1])
-                n.append(tile_im_array[y,x+1])
-
-                # set the pixel's colour to be the channel-wise mean of its neighbours
-                neighbours_a = np.array(n)
-                tile_im_array[y,x] = np.mean(neighbours_a, axis=0)
-
-    return tile_im_array
 
 def render_frame(frame_id, frame_df):
     # assign a tile_id and a pixel x value to each raw point
@@ -108,9 +61,6 @@ def render_frame(frame_id, frame_df):
             y = r[1]
             c = r[2]
             tile_im_array[y,x,:] = c
-
-        # fill in zero pixels with interpolated values
-        # tile_im_array = interpolate_pixels(tile_im_array)
 
         # create an image of the intensity array
         tile = Image.fromarray(tile_im_array, 'RGB')
