e0a7ba0029ea534916d791b5d93518cf88701ccf
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2020-06-14 11:40:29 +1000

    Update frame-publisher.swift

--- yolo/jetson/frame-publisher.swift
+++ yolo/jetson/frame-publisher.swift
@@ -5,16 +5,3 @@ let PUBLISHED_FRAMES_DIR = "/data/published-frames"
 
 // Configure a SQLite database
 let sqlite = try SQLiteDatabase(storage: .file(path: CONVERTED_DATABASE_NAME))
-
-struct SQLiteVersion: Codable {
-    let version: String
-}
-
-/// Register the configured SQLite database to the database config.
-var databases = DatabasesConfig()
-databases.add(database: sqlite, as: .sqlite)
-services.register(databases)
-
-conn.select()
-            .column(function: "sqlite_version", as: "version")
-            .all(decoding: SQLiteVersion.self)
