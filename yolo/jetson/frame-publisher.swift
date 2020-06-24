fbe50327c1ca01688ffcf66a275241c12b59c88d
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2020-06-24 15:32:00 +1000

    Delete frame-publisher.swift

--- yolo/jetson/frame-publisher.swift
+++
@@ -1,7 +0,0 @@
-import SQLite
-
-let CONVERTED_DATABASE_NAME = "/data/exp-dwm-test-run-190719_Hela_Ecoli_1to1_01-converted.sqlite"
-let PUBLISHED_FRAMES_DIR = "/data/published-frames"
-
-// Configure a SQLite database
-let sqlite = try SQLiteDatabase(storage: .file(path: CONVERTED_DATABASE_NAME))
