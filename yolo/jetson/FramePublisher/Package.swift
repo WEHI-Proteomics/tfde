80dabc6018d9ad74482a0723efe0388d43344952
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2020-06-25 19:25:55 +1000

    Updated.

--- yolo/jetson/FramePublisher/Package.swift
+++ yolo/jetson/FramePublisher/Package.swift
@@ -9,14 +9,15 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.12.0"),
-        .package(url: "https://github.com/twostraws/SwiftGD.git", from: "2.0.0")
+        .package(url: "https://github.com/twostraws/SwiftGD.git", from: "2.0.0"),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "4.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FramePublisher",
-            dependencies: ["SQLite","SwiftGD"]),
+            dependencies: ["SQLite","SwiftGD","SwiftyJSON"]),
         .testTarget(
             name: "FramePublisherTests",
             dependencies: ["FramePublisher"]),
--- yolo/jetson/FramePublisher/Sources/FramePublisher/main.swift
+++ yolo/jetson/FramePublisher/Sources/FramePublisher/main.swift
@@ -1,6 +1,7 @@
 import Foundation
 import SQLite
 import SwiftGD
+import SwiftyJSON
 
 #if os(Linux)
 let CONVERTED_DATABASE_NAME = "/data/exp-dwm-test-run-190719_Hela_Ecoli_1to1_01-converted.sqlite"
@@ -21,8 +22,8 @@ let TILES_PER_FRAME = Int((MZ_MAX - MZ_MIN) / MZ_PER_TILE) + 1
 let MIN_TILE_IDX = 0
 let MAX_TILE_IDX = TILES_PER_FRAME-1
 
-let MINIMUM_PIXEL_INTENSITY = 1
-let MAXIMUM_PIXEL_INTENSITY = 1000
+let MINIMUM_PIXEL_INTENSITY: Int64 = 1
+let MAXIMUM_PIXEL_INTENSITY: Int64 = 1000
 
 
 func greet(person: String) -> String {
@@ -30,16 +31,18 @@ func greet(person: String) -> String {
     return greeting
 }
 
-func tileAndPixelXFromMz(mz: Double) -> (Int, Int) {
-    let mz_adj = mz - MZ_MIN
-    let tile_id = Int(mz_adj / MZ_PER_TILE)
-    let pixel_x = Int((mz_adj % MZ_PER_TILE) / MZ_PER_TILE * PIXELS_X)
-    return (tile_id, pixel_x)
+func tileAndPixelXFromMz(mz: Double) -> (Int64, Int64) {
+    let mzAdj = mz - MZ_MIN
+    let tileId = Int64(mzAdj / MZ_PER_TILE)
+    let pixelX = Int64((mzAdj.truncatingRemainder(dividingBy: MZ_PER_TILE)) / MZ_PER_TILE * Double(PIXELS_X))
+    return (tileId, pixelX)
 }
 
-// figure out where to save our file
-let currentDirectory = URL(fileURLWithPath: FileManager().currentDirectoryPath)
-let destination = currentDirectory.appendingPathComponent("output-1.png")
+struct PixelColour {
+    var red: Double
+    var green: Double
+    var blue: Double
+}
 
 struct TilePixel {
     var mz: Double
@@ -47,10 +50,22 @@ struct TilePixel {
     var intensity: Int64
     var tileId: Int64
     var pixelX: Int64
-    var colourR: Double
-    var colourG: Double
-    var colourB: Double
 }
+
+struct GroupedTilePixel {
+    var tileId: Int64
+    var pixelX: Int64
+    var scan: Int64
+    var intensity: Int64
+    var colour: PixelColour
+}
+
+struct IntensityColourMapping {
+    var intensity: Int64
+    var colour: PixelColour
+}
+
+let clipped = PixelColour(red: 1.0, green: 0.0, blue:0.0)
 
 let frames = Table("frames")
 let frameId = Expression<Int64>("frame_id")
@@ -58,29 +73,70 @@ let mz = Expression<Double>("mz")
 let scan = Expression<Int64>("scan")
 let intensity = Expression<Int64>("intensity")
 
+// load colour table
+let path = URL(fileURLWithPath: "/Users/darylwilding-mcbride/Downloads/colourmap.json")
+let data = try Data(contentsOf: path, options: .mappedIfSafe)
+let json = try JSON(data: data)
+
+var colourLookup: [IntensityColourMapping] = []
+for (_, subJson) in json {
+    let colour: JSON = subJson["colour"]
+    let r: Double = colour[0].double ?? 0.0 / 255.0
+    let g: Double = colour[1].double ?? 0.0 / 255.0
+    let b: Double = colour[2].double ?? 0.0 / 255.0
+
+    let intensity: Int64 = subJson["intensity"].int64 ?? 0
+
+    let intensityColour = IntensityColourMapping(intensity: intensity, colour: PixelColour(red: r, green: g, blue: b))
+    colourLookup.append(intensityColour)
+}
 
 // build the array of tile pixels
 print("reading the database")
 var tilePixels: [TilePixel] = []
 for frame in try db.prepare(frames.filter(frameId == 1899)) {
-    // print("id: \(frame[frameId]), m/z: \(frame[mz]), scan: \(frame[scan])")
-    let pixel = TilePixel(mz: frame[mz], scan: frame[scan], intensity: frame[intensity], tileId:0, pixelX: 0, colourR: 0.2, colourG: 0.4, colourB: 0.8)
+    let (tileId, pixelX) = tileAndPixelXFromMz(mz: frame[mz])
+    let pixel = TilePixel(mz: frame[mz], scan: frame[scan], intensity: frame[intensity], tileId:tileId, pixelX: pixelX)
     tilePixels.append(pixel)
 }
 
 
-// if let image = Image(width: PIXELS_X, height: PIXELS_Y) {
-//     // for each 
-//     for p in try db.prepare(query) {
-//         print("m/z: \(p[0] ?? 0.0), scan: \(p[1] ?? 0.0), intensity: \(p[2] ?? 0)")
-//     }
+// sum the intensity values in the same (tile,scan,pixelX)
+let dict = Dictionary(grouping: tilePixels) { [$0.tileId, $0.pixelX, $0.scan] }
+var groupedTilePixels: [GroupedTilePixel] = []
+for (key,value) in dict{
+    // calculate the total intensity for this pixel
+    var totalIntensity = 0
+    for o in value {
+        totalIntensity += Int(o.intensity)
+    }
+
+    // look up the colour for this intensity
+    var intensityColourMapping: IntensityColourMapping = IntensityColourMapping(intensity: MAXIMUM_PIXEL_INTENSITY, colour: clipped)
+    if totalIntensity <= MAXIMUM_PIXEL_INTENSITY {
+        intensityColourMapping = colourLookup[totalIntensity-1]
+    }
+    let colour = intensityColourMapping.colour
+    let pixelColour = PixelColour(red: colour.red, green: colour.green, blue: colour.blue)
+
+    groupedTilePixels.append(GroupedTilePixel(tileId: key[0], pixelX: key[1], scan: key[2], intensity: Int64(totalIntensity), colour: pixelColour))
+}
 
 
-//     // image.set(pixel: Point(x: x, y: y), to: Color(red: 0.6, green: 0.8, blue: 0.2, alpha: 1))
+for tileId in MIN_TILE_IDX...MAX_TILE_IDX {
+    let tilePath = URL(fileURLWithPath: "/Users/darylwilding-mcbride/Downloads/swift-tiles/tile-\(tileId).png")
 
+    let filtered = groupedTilePixels.filter{ $0.tileId == tileId } 
+    if let image = Image(width: PIXELS_X, height: PIXELS_Y) {
+        // set the pixels in the image
+        for pixel in filtered {
+            let c = Color(red: pixel.colour.red, green: pixel.colour.green, blue: pixel.colour.blue, alpha: 1)
+            image.set(pixel: Point(x: Int(pixel.pixelX), y: Int(pixel.scan)), to: c)
+        }
 
-//     // save the final image to disk
-//     image.write(to: destination)
-// }
+        // save the final image to disk
+        image.write(to: tilePath)
+    }
+}
 
 print("finished")
