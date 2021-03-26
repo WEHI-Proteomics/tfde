81ce03f080e587652cc89346a3847a37700231fa
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2021-03-26 13:22:32 +1100

    Added feature de-dup as an option.

--- pipeline/detect-features.py
+++ pipeline/detect-features.py
@@ -287,6 +287,7 @@ parser.add_argument('-pid', '--precursor_id', type=int, help='Only process this 
 parser.add_argument('-rm','--ray_mode', type=str, choices=['local','cluster'], help='The Ray mode to use.', required=True)
 parser.add_argument('-pc','--proportion_of_cores_to_use', type=float, default=0.9, help='Proportion of the machine\'s cores to use for this program.', required=False)
 parser.add_argument('-v','--visualise', action='store_true', help='Generate data for visualisation of the feature detection.')
+parser.add_argument('-rd','--remove_duplicates', action='store_true', help='Remove duplicated features.')
 args = parser.parse_args()
 
 # Print the arguments for the log
@@ -335,6 +336,9 @@ MIN_SCORE_MS1_DECONVOLUTION_FEATURE = cfg.getfloat('ms1', 'MIN_SCORE_MS1_DECONVO
 MIN_SCORE_MS2_DECONVOLUTION_FEATURE = cfg.getfloat('ms2', 'MIN_SCORE_MS2_DECONVOLUTION_FEATURE')
 FEATURE_DETECTION_MIN_CHARGE = cfg.getint('ms1', 'FEATURE_DETECTION_MIN_CHARGE')
 FEATURE_DETECTION_MAX_CHARGE = cfg.getint('ms1', 'FEATURE_DETECTION_MAX_CHARGE')
+DUP_MZ_TOLERANCE_PPM = cfg.getint('ms1', 'DUP_MZ_TOLERANCE_PPM')
+DUP_SCAN_TOLERANCE = cfg.getint('ms1', 'DUP_SCAN_TOLERANCE')
+DUP_RT_TOLERANCE = cfg.getint('ms1', 'DUP_RT_TOLERANCE')
 
 # input cuboids
 CUBOIDS_DIR = "{}/precursor-cuboids-{}".format(EXPERIMENT_DIR, args.precursor_definition_method)
@@ -390,6 +394,59 @@ info.append(('processed', time.ctime()))
 content_d = {'features_df':features_df, 'metadata':info}
 with open(FEATURES_FILE, 'wb') as handle:
     pickle.dump(content_d, handle)
+
+# de-dup the features if required
+if args.remove_duplicates:
+    dedup_start_run = time.time()
+
+    # set up dup definitions
+    MZ_TOLERANCE_PERCENT = DUP_MZ_TOLERANCE_PPM * 10**-4
+    features_df['dup_mz'] = features_df['monoisotopic_mz']  # shorthand to reduce verbosity
+    features_df['dup_mz_ppm_tolerance'] = features_df.dup_mz * MZ_TOLERANCE_PERCENT / 100
+    features_df['dup_mz_lower'] = features_df.dup_mz - features_df.dup_mz_ppm_tolerance
+    features_df['dup_mz_upper'] = features_df.dup_mz + features_df.dup_mz_ppm_tolerance
+    features_df['dup_scan_lower'] = features_df.scan_apex - DUP_SCAN_TOLERANCE
+    features_df['dup_scan_upper'] = features_df.scan_apex + DUP_SCAN_TOLERANCE
+    features_df['dup_rt_lower'] = features_df.rt_apex - DUP_RT_TOLERANCE
+    features_df['dup_rt_upper'] = features_df.rt_apex + DUP_RT_TOLERANCE
+
+    # remove these after we're finished
+    columns_to_drop_l = []
+    columns_to_drop_l.append('dup_mz')
+    columns_to_drop_l.append('dup_mz_ppm_tolerance')
+    columns_to_drop_l.append('dup_mz_lower')
+    columns_to_drop_l.append('dup_mz_upper')
+    columns_to_drop_l.append('dup_scan_lower')
+    columns_to_drop_l.append('dup_scan_upper')
+    columns_to_drop_l.append('dup_rt_lower')
+    columns_to_drop_l.append('dup_rt_upper')
+
+    # see if any detections have a duplicate - if so, find the dup with the highest intensity and keep it
+    keep_l = []
+    for row in features_df.itertuples():
+        dup_df = features_df[(features_df.dup_mz > row.dup_mz_lower) & (features_df.dup_mz < row.dup_mz_upper) & (features_df.scan_apex > row.dup_scan_lower) & (features_df.scan_apex < row.dup_scan_upper) & (features_df.rt_apex > row.dup_rt_lower) & (features_df.rt_apex < row.dup_rt_upper)].copy()
+        # group the dups by charge - take the most intense for each charge
+        for group_name,group_df in dup_df.groupby(['charge'], as_index=False):
+            keep_l.append(group_df.iloc[0].feature_id)
+
+    # remove any features that are not in the keep list
+    dedup_df = features_df[features_df.feature_id.isin(keep_l)].copy()
+
+    number_of_dups = len(features_df)-len(dedup_df)
+    print('removed {} duplicates ({}% of the original detections)'.format(number_of_dups, round(number_of_dups/len(features_df)*100)))
+    print('there are {} detected de-duplicated features'.format(len(dedup_df)))
+
+    # remove the columns we added earlier
+    dedup_df.drop(columns_to_drop_l, axis=1, inplace=True)
+
+    FEATURES_DEDUP_FILE = '{}/exp-{}-run-{}-features-{}-dedup.pkl'.format(FEATURES_DIR, args.experiment_name, args.run_name, args.feature_detection_method)
+
+    # write out all the features
+    print("writing {} de-duped features to {}".format(len(dedup_df), FEATURES_DEDUP_FILE))
+    info.append(('dedup_running_time',round(time.time()-dedup_start_run,1)))
+    content_d = {'features_df':dedup_df, 'metadata':info}
+    with open(FEATURES_DEDUP_FILE, 'wb') as handle:
+        pickle.dump(content_d, handle)
 
 stop_run = time.time()
 print("total running time ({}): {} seconds".format(parser.prog, round(stop_run-start_run,1)))
