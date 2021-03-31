66372b9d399466030c7aaa60327f5f77308ba16f
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2021-03-31 15:43:46 +1100

    Update search-mgf.py

--- pipeline/search-mgf.py
+++ pipeline/search-mgf.py
@@ -22,6 +22,7 @@ parser.add_argument('-rn','--run_name', type=str, help='Name of the run.', requi
 parser.add_argument('-fdm','--feature_detection_method', type=str, choices=['pasef','3did'], help='Which feature detection method.', required=True)
 parser.add_argument('-ff','--fasta_file_name', type=str, default='./otf-peak-detect/fasta/Human_Yeast_Ecoli.fasta', help='File name of the FASTA file.', required=False)
 parser.add_argument('-cp','--comet_parameter_file_name', type=str, default='./otf-peak-detect/comet/TimsTOF.params', help='File name of the Comet parameter file.', required=False)
+parser.add_argument('-recal','--recalibration_mode', action='store_true', help='Use the recalibrated MGF.')
 args = parser.parse_args()
 
 # Print the arguments for the log
@@ -45,16 +46,19 @@ if not os.path.exists(MGF_DIR):
     sys.exit(1)
 
 # check the MGF file
-MGF_FILE = '{}/exp-{}-run-{}-features-{}.mgf'.format(MGF_DIR, args.experiment_name, args.run_name, args.feature_detection_method)
+if not args.recalibration_mode:
+    MGF_FILE = '{}/exp-{}-run-{}-features-{}-recalibrated.mgf'.format(MGF_DIR, args.experiment_name, args.run_name, args.feature_detection_method)
+else:
+    MGF_FILE = '{}/exp-{}-run-{}-features-{}.mgf'.format(MGF_DIR, args.experiment_name, args.run_name, args.feature_detection_method)
+
 if not os.path.isfile(MGF_FILE):
     print("The MGF file is required but doesn't exist: {}".format(MGF_FILE))
     sys.exit(1)
 
 # set up the Comet output directory
 COMET_OUTPUT_DIR = "{}/comet-output-{}".format(EXPERIMENT_DIR, args.feature_detection_method)
-if os.path.exists(COMET_OUTPUT_DIR):
-    shutil.rmtree(COMET_OUTPUT_DIR)
-os.makedirs(COMET_OUTPUT_DIR)
+if not os.path.exists(COMET_OUTPUT_DIR):
+    os.makedirs(COMET_OUTPUT_DIR)
 
 # run comet on it
 cmd = "{}/crux-3.2.Linux.x86_64/bin/crux comet --parameter-file {} --output-dir {} --fileroot \"{}\" {} {}".format(os.getcwd(), args.comet_parameter_file_name, COMET_OUTPUT_DIR, args.run_name, MGF_FILE, args.fasta_file_name)
