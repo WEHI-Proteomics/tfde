be3a838ecbce9e549e936e0ff86dee806fe56102
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2021-03-31 17:26:11 +1100

    Update search-mgf.py

--- pipeline/search-mgf.py
+++ pipeline/search-mgf.py
@@ -21,8 +21,8 @@ parser.add_argument('-en','--experiment_name', type=str, help='Name of the exper
 parser.add_argument('-rn','--run_name', type=str, help='Name of the run.', required=True)
 parser.add_argument('-fdm','--feature_detection_method', type=str, choices=['pasef','3did'], help='Which feature detection method.', required=True)
 parser.add_argument('-ff','--fasta_file_name', type=str, default='./otf-peak-detect/fasta/Human_Yeast_Ecoli.fasta', help='File name of the FASTA file.', required=False)
-parser.add_argument('-cp','--comet_parameter_file_name', type=str, default='./otf-peak-detect/comet/TimsTOF.params', help='File name of the Comet parameter file.', required=False)
 parser.add_argument('-recal','--recalibration_mode', action='store_true', help='Use the recalibrated MGF.')
+parser.add_argument('-ini','--ini_file', type=str, default='./otf-peak-detect/pipeline/pasef-process-short-gradient.ini', help='Path to the config file.', required=False)
 args = parser.parse_args()
 
 # Print the arguments for the log
@@ -39,6 +39,19 @@ if not os.path.exists(EXPERIMENT_DIR):
     print("The experiment directory is required but doesn't exist: {}".format(EXPERIMENT_DIR))
     sys.exit(1)
 
+# check the INI file exists
+if not os.path.isfile(args.ini_file):
+    print("The configuration file doesn't exist: {}".format(args.ini_file))
+    sys.exit(1)
+
+# load the INI file
+cfg = configparser.ConfigParser(interpolation=ExtendedInterpolation())
+cfg.read(args.ini_file)
+
+# set up constants
+INITIAL_SEARCH_PARAMETERS = cfg.get('comet','INITIAL_SEARCH_PARAMETERS')
+RECALIBRATED_SEARCH_PARAMETERS = cfg.get('comet','RECALIBRATED_SEARCH_PARAMETERS')
+
 # check the MGF directory
 MGF_DIR = "{}/mgf-{}".format(EXPERIMENT_DIR, args.feature_detection_method)
 if not os.path.exists(MGF_DIR):
@@ -49,9 +62,14 @@ if not os.path.exists(MGF_DIR):
 if not args.recalibration_mode:
     MGF_FILE = '{}/exp-{}-run-{}-features-{}.mgf'.format(MGF_DIR, args.experiment_name, args.run_name, args.feature_detection_method)
     COMET_OUTPUT_DIR = "{}/comet-output-{}".format(EXPERIMENT_DIR, args.feature_detection_method)
+    COMET_PARAMS = INITIAL_SEARCH_PARAMETERS
 else:
     MGF_FILE = '{}/exp-{}-run-{}-features-{}-recalibrated.mgf'.format(MGF_DIR, args.experiment_name, args.run_name, args.feature_detection_method)
     COMET_OUTPUT_DIR = "{}/comet-output-{}-recalibrated".format(EXPERIMENT_DIR, args.feature_detection_method)
+    COMET_PARAMS = RECALIBRATED_SEARCH_PARAMETERS
+
+file_directory = os.path.abspath(os.path.dirname(__file__))
+COMET_PARAM_FILE = '{}/../comet/{}'.format(file_directory, COMET_PARAMS)
 
 # check the MGF file
 if not os.path.isfile(MGF_FILE):
@@ -63,7 +81,7 @@ if not os.path.exists(COMET_OUTPUT_DIR):
     os.makedirs(COMET_OUTPUT_DIR)
 
 # run comet on it
-cmd = "{}/crux-3.2.Linux.x86_64/bin/crux comet --parameter-file {} --output-dir {} --fileroot \"{}\" {} {}".format(os.getcwd(), args.comet_parameter_file_name, COMET_OUTPUT_DIR, args.run_name, MGF_FILE, args.fasta_file_name)
+cmd = "{}/crux-3.2.Linux.x86_64/bin/crux comet --parameter-file {} --output-dir {} --fileroot \"{}\" {} {}".format(os.getcwd(), COMET_PARAM_FILE, COMET_OUTPUT_DIR, args.run_name, MGF_FILE, args.fasta_file_name)
 run_process(cmd)
 
 stop_run = time.time()
