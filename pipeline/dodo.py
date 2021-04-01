0e3bf1988e257b904044742ce428775366bee265
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2021-04-01 14:10:48 +1100

    Create dodo.py

---
+++ pipeline/dodo.py
@@ -0,0 +1,11 @@
+def task_hello():
+    """hello"""
+
+    def python_hello(targets):
+        with open(targets[0], "a") as output:
+            output.write("Python says Hello World!!!\n")
+
+    return {
+        'actions': [python_hello],
+        'targets': ["hello.txt"],
+        }
