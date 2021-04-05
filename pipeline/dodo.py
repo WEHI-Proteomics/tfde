b7727d081a69faa844b76fb65df53eff78093de4
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2021-04-05 11:41:04 +1000

    Update dodo.py

--- pipeline/dodo.py
+++ pipeline/dodo.py
@@ -2,7 +2,7 @@
 
 def task_identify_searched_features():
     return {
-        'file_dep': ['pipline/identify-searched-features.py'],
+        'file_dep': ['pipeline/identify-searched-features.py'],
         'actions': ['python -u ./otf-peak-detect/pipeline/identify-features.py -eb /media/big-ssd/experiments -en P3856 -fdm pasef -ns -recal'],
         'targets': ['/media/big-ssd/experiments/P3856/identifications-pasef/exp-P3856-identifications-pasef-recalibrated.pkl']
     }
