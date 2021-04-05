8abc63b4bd65577826887e4b8471546adc177326
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2021-04-05 11:39:47 +1000

    Update dodo.py

--- pipeline/dodo.py
+++ pipeline/dodo.py
@@ -1,11 +1,8 @@
-def task_hello():
-    """hello"""
+# This is the set of tasks to take a raw instrument database and create a list of peptides
 
-    def python_hello(targets):
-        with open(targets[0], "a") as output:
-            output.write("Python says Hello World!!!\n")
-
+def task_identify_searched_features():
     return {
-        'actions': [python_hello],
-        'targets': ["hello.txt"],
-        }
+        'file_dep': ['pipline/identify-searched-features.py'],
+        'actions': ['python -u ./otf-peak-detect/pipeline/identify-features.py -eb /media/big-ssd/experiments -en P3856 -fdm pasef -ns -recal'],
+        'targets': ['/media/big-ssd/experiments/P3856/identifications-pasef/exp-P3856-identifications-pasef-recalibrated.pkl']
+    }
