bcab177b24fb6eb310ac5cc06b131b60645b818c
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2021-04-05 12:23:49 +1000

    Update dodo.py

--- pipeline/dodo.py
+++ pipeline/dodo.py
@@ -4,5 +4,6 @@ def task_identify_searched_features():
     return {
         'file_dep': ['identify-searched-features.py'],
         'actions': ['python -u identify-searched-features.py -eb /media/big-ssd/experiments -en P3856 -ini pasef-process-short-gradient.ini -ff ../fasta/Human_Yeast_Ecoli.fasta -fdm pasef -ns -recal'],
-        'targets': ['/media/big-ssd/experiments/P3856/identifications-pasef/exp-P3856-identifications-pasef-recalibrated.pkl']
+        'targets': ['/media/big-ssd/experiments/P3856/identifications-pasef/exp-P3856-identifications-pasef-recalibrated.pkl'],
+        'verbosity': 2
     }
