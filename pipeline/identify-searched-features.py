6de5e89e6fc9f7b0d7f5a143b326d61b70464b20
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2021-04-05 12:20:55 +1000

    Assume that crux is installed in the user's home directory.

--- pipeline/identify-searched-features.py
+++ pipeline/identify-searched-features.py
@@ -8,6 +8,7 @@ import pandas as pd
 import pickle
 import configparser
 from configparser import ExtendedInterpolation
+from os.path import expanduser
 
 # run the command in a shell
 def run_process(process):
@@ -83,7 +84,7 @@ os.makedirs(PERCOLATOR_OUTPUT_DIR)
 PERCOLATOR_STDOUT_FILE_NAME = "{}/percolator-stdout.log".format(PERCOLATOR_OUTPUT_DIR)
 comet_output_file_list = glob.glob('{}/*.comet.target.pin'.format(COMET_OUTPUT_DIR))
 comet_output_file_list_as_string = ' '.join(map(str, comet_output_file_list))
-cmd = "{}/crux-3.2.Linux.i686/bin/crux percolator --overwrite T --subset-max-train 1000000 --klammer F --maxiter 10 --output-dir {} --picked-protein {} --protein T --protein-enzyme {} --search-input auto --verbosity 30 --fileroot {} {} > {} 2>&1".format(os.getcwd(), PERCOLATOR_OUTPUT_DIR, args.fasta_file_name, args.protein_enzyme, args.experiment_name, comet_output_file_list_as_string, PERCOLATOR_STDOUT_FILE_NAME)
+cmd = "{}/crux-3.2.Linux.i686/bin/crux percolator --overwrite T --subset-max-train 1000000 --klammer F --maxiter 10 --output-dir {} --picked-protein {} --protein T --protein-enzyme {} --search-input auto --verbosity 30 --fileroot {} {} > {} 2>&1".format(expanduser("~"), PERCOLATOR_OUTPUT_DIR, args.fasta_file_name, args.protein_enzyme, args.experiment_name, comet_output_file_list_as_string, PERCOLATOR_STDOUT_FILE_NAME)
 run_process(cmd)
 
 # determine the mapping between the percolator index and the run file name - this is only available by parsing percolator's stdout redirected to a text file.
