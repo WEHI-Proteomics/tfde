1346b50927f1841e825b7272fe299a4f8aa8da22
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2021-04-06 09:15:46 +1000

    Update detect-features.py

--- pipeline/detect-features.py
+++ pipeline/detect-features.py
@@ -266,7 +266,7 @@ def resolve_fragment_ions(feature_d, ms2_points_df):
 
 # save visualisation data for later analysis of how feature detection works
 def save_visualisation(visualise_d):
-    precursor_cuboid_id = precursor_cuboid_d['precursor_cuboid_id']
+    precursor_cuboid_id = visualise_d['precursor_cuboid_d']['precursor_cuboid_id']
     VIS_FILE = '{}/feature-detection-visualisation-{}.pkl'.format(expanduser("~"), precursor_cuboid_id)
     print("writing feature detection visualisation data to {}".format(VIS_FILE))
     with open(VIS_FILE, 'wb') as handle:
