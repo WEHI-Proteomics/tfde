f7162c7afa1f3a881c8b1498ef4377878fdd9449
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2021-04-26 11:55:21 +1000

    Update remove-duplicate-features.py

--- pipeline/remove-duplicate-features.py
+++ pipeline/remove-duplicate-features.py
@@ -13,6 +13,7 @@ parser.add_argument('-eb','--experiment_base_dir', type=str, default='./experime
 parser.add_argument('-en','--experiment_name', type=str, help='Name of the experiment.', required=True)
 parser.add_argument('-rn','--run_name', type=str, help='Name of the run.', required=True)
 parser.add_argument('-pdm','--precursor_definition_method', type=str, choices=['pasef','3did'], help='The method used to define the precursor cuboids.', required=True)
+parser.add_argument('-ini','--ini_file', type=str, default='./otf-peak-detect/pipeline/pasef-process-short-gradient.ini', help='Path to the config file.', required=False)
 args = parser.parse_args()
 
 # Print the arguments for the log
