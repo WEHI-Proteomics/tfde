ebc2d36774f428b0357117cfaa5b350d955069f0
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2021-05-29 18:23:05 +1000

    Update detect-features-with-3did.py

--- 3did/detect-features-with-3did.py
+++ 3did/detect-features-with-3did.py
@@ -229,7 +229,7 @@ def calculate_monoisotopic_mass_from_mz(monoisotopic_mz, charge):
     return monoisotopic_mass
 
 # determine the voxels to be removed
-def voxels_to_remove(points_df, voxels_df):
+def voxels_for_feature(points_df, voxels_df):
     # calculate the intensity contribution of the points to their voxel's intensity
     df = points_df.groupby(['bin_key'], as_index=False, sort=False).intensity.agg(['sum','count']).reset_index()
     df.rename(columns={'sum':'intensity', 'count':'point_count'}, inplace=True)
@@ -237,7 +237,7 @@ def voxels_to_remove(points_df, voxels_df):
     df['proportion'] = df.intensity_feature / df.intensity_voxel
     # if the feature points comprise most of a voxel's intensity, we don't need to process that voxel later on
     df = df[(df.proportion >= 0.8)]
-    return set(df.bin_key)
+    return set(df.voxel_id.tolist())
 
 # generate a unique feature_id from the precursor id and the feature sequence number found for that precursor
 def generate_voxel_id(segment_id, voxel_sequence_number):
@@ -279,6 +279,9 @@ def find_features(segment_mz_lower, segment_mz_upper, segment_id):
         summary_df.dropna(subset=['intensity'], inplace=True)
         summary_df = summary_df[(summary_df.intensity >= MINIMUM_VOXEL_INTENSITY)]
         summary_df.sort_values(by=['intensity'], ascending=False, inplace=True)
+        summary_df.reset_index(drop=True, inplace=True)
+        summary_df['voxel_id'] = summary_df.index
+        summary_df['voxel_id'] = summary_df.apply(lambda row: generate_voxel_id(segment_id, row.voxel_id+1), axis=1)
         print('there are {} voxels for processing in segment {}'.format(len(summary_df), segment_id))
 
         # keep track of the keys of voxels that have been processed
@@ -287,7 +290,7 @@ def find_features(segment_mz_lower, segment_mz_upper, segment_id):
         # process each voxel by decreasing intensity
         for voxel_idx,row in enumerate(summary_df.itertuples()):
             # if this voxel hasn't already been processed...
-            if (row.bin_key not in voxels_processed):
+            if (row.voxel_id not in voxels_processed):
                 # retrieve the bins from the voxel key
                 (mz_bin, scan_bin, rt_bin) = row.bin_key
 
@@ -488,7 +491,6 @@ def find_features(segment_mz_lower, segment_mz_upper, segment_id):
                                 # record the feature region where we found this feature
                                 feature_d['feature_region_3d_extent'] = feature_region_3d_extent_d
                                 # record the voxel from where we derived the initial isotope
-                                feature_d['voxel_id'] = generate_voxel_id(segment_id, voxel_idx+1)
                                 feature_d['voxel_metadata_d'] = voxel_metadata_d
                                 feature_d['scan_df'] = scan_df.to_dict('records')
                                 feature_d['rt_df'] = rt_df.to_dict('records')
@@ -499,10 +501,11 @@ def find_features(segment_mz_lower, segment_mz_upper, segment_id):
                                 feature_points_df = feature_region_3d_df[(feature_region_3d_df.mz >= row.envelope[0][0]-0.1) & (feature_region_3d_df.mz <= row.envelope[-1][0]+0.1) & (feature_region_3d_df.scan >= feature_d['scan_lower']) & (feature_region_3d_df.scan <= feature_d['scan_upper']) & (feature_region_3d_df.retention_time_secs >= feature_d['rt_lower']) & (feature_region_3d_df.retention_time_secs <= feature_d['rt_upper'])]
 
                                 # add the points' voxel to the list of processed voxels if the proportion of their intensity is above a threshold
-                                bin_key_set = voxels_to_remove(points_df=feature_points_df, voxels_df=summary_df)
+                                voxel_ids_for_feature = voxels_for_feature(points_df=feature_points_df, voxels_df=summary_df)
 
                                 # add the voxels included in the feature's points to the list of voxels already processed
-                                voxels_processed.update(bin_key_set)
+                                voxels_processed.update(set(voxel_ids_for_feature))
+                                
                         print('.', end='', flush=True)
                     else:
                         print('f', end='', flush=True)
