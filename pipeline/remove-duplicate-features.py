8afc2cf09b1d8380805e64da234ad13109c63691
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2021-06-01 09:44:41 +1000

    Separate dup tolerance for 3DID.

--- pipeline/remove-duplicate-features.py
+++ pipeline/remove-duplicate-features.py
@@ -40,9 +40,14 @@ cfg = configparser.ConfigParser(interpolation=ExtendedInterpolation())
 cfg.read(args.ini_file)
 
 # set up constants
-DUP_MZ_TOLERANCE_PPM = cfg.getint('ms1', 'DUP_MZ_TOLERANCE_PPM')
-DUP_SCAN_TOLERANCE = cfg.getint('ms1', 'DUP_SCAN_TOLERANCE')
-DUP_RT_TOLERANCE = cfg.getint('ms1', 'DUP_RT_TOLERANCE')
+if args.precursor_definition_method == '3did':
+    DUP_MZ_TOLERANCE_PPM = cfg.getint('3did', 'DUP_MZ_TOLERANCE_PPM')
+    DUP_SCAN_TOLERANCE = cfg.getint('3did', 'DUP_SCAN_TOLERANCE')
+    DUP_RT_TOLERANCE = cfg.getint('3did', 'DUP_RT_TOLERANCE')
+else:
+    DUP_MZ_TOLERANCE_PPM = cfg.getint('ms1', 'DUP_MZ_TOLERANCE_PPM')
+    DUP_SCAN_TOLERANCE = cfg.getint('ms1', 'DUP_SCAN_TOLERANCE')
+    DUP_RT_TOLERANCE = cfg.getint('ms1', 'DUP_RT_TOLERANCE')
 
 # input features
 FEATURES_DIR = "{}/features-{}".format(EXPERIMENT_DIR, args.precursor_definition_method)
