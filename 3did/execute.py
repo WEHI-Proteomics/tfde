e18794648798b9ebefbd443a0cb95ad0dc29cf4d
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2021-07-29 10:25:46 +1000

    Update execute.py

--- 3did/execute.py
+++ 3did/execute.py
@@ -17,6 +17,7 @@ config = {
     'experiment_base_dir': get_var('eb', '/media/big-ssd/experiments'),
     'experiment_name': get_var('en', None),
     'run_name': get_var('rn', None),
+    'raw_database_name': get_var('rdn', None),
     'ini_file': get_var('ini', ini_file),
     'proportion_of_cores_to_use': get_var('pc', 0.8),
     'minvi': get_var('minvi', 3000)
@@ -29,13 +30,6 @@ EXPERIMENT_DIR = "{}/{}".format(config['experiment_base_dir'], config['experimen
 start_run = time.time()
 
 
-
-####################
-# raw conversion (TO BE ADDED)
-####################
-
-
-
 ####################
 # feature extraction
 ####################
@@ -43,7 +37,7 @@ def task_detect_features():
     # input
     CONVERTED_DATABASE_NAME = "{experiment_dir}/converted-databases/exp-{experiment_name}-run-{run_name}-converted.sqlite".format(experiment_dir=EXPERIMENT_DIR, experiment_name=config['experiment_name'], run_name=config['run_name'])
     # command
-    cmd = 'python -u detect-features-with-3did.py -eb {experiment_base} -en {experiment_name} -rn {run_name} -ini {INI_FILE} -rm cluster -minvi {minvi}'.format(experiment_base=config['experiment_base_dir'], experiment_name=config['experiment_name'], run_name=config['run_name'], INI_FILE=config['ini_file'], minvi=config['minvi'])
+    cmd = 'python -u detect-features-with-3did.py -eb {experiment_base} -en {experiment_name} -rdn {raw_database_name} -rn {run_name} -ini {INI_FILE} -rm cluster -minvi {minvi}'.format(experiment_base=config['experiment_base_dir'], experiment_name=config['experiment_name'], raw_database_name=config['raw_database_name'], run_name=config['run_name'], INI_FILE=config['ini_file'], minvi=config['minvi'])
     # output
     FEATURES_DIR = '{experiment_dir}/features-3did'.format(experiment_dir=EXPERIMENT_DIR)
     FEATURES_FILE = '{features_dir}/exp-{experiment_name}-run-{run_name}-features-3did.pkl'.format(features_dir=FEATURES_DIR, experiment_name=config['experiment_name'], run_name=config['run_name'])
