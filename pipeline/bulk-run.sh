c52f981d44c0a4e48092d6ff17df2f3aae369746
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2021-08-27 18:49:47 +1000

    Update bulk-run.sh

--- pipeline/bulk-run.sh
+++ pipeline/bulk-run.sh
@@ -2,29 +2,25 @@
 
 # P3830
 #
-# cs true, fmdw false
-# echo "P3830, cs true, fmdw false"
-# doit -f ./otf-peak-detect/pipeline/execute-run.py clean en=P3830
-# doit -f ./otf-peak-detect/pipeline/execute-run.py pc=0.8 cs=true fmdw=false en=P3830 rn=P3830_YeastUPS2_01_Slot1-1_1_5082 rl=0 ru=3000 ff="~/otf-peak-detect/fasta/ups1-ups2-yeast.fasta"
-# echo
-
-# cs false, fmdw false
-# echo "P3830, cs false, fmdw false"
-# doit -f ./otf-peak-detect/pipeline/execute-run.py clean en=P3830
-# doit -f ./otf-peak-detect/pipeline/execute-run.py pc=0.8 cs=false fmdw=false en=P3830 rn=P3830_YeastUPS2_01_Slot1-1_1_5082 rl=0 ru=3000 ff="~/otf-peak-detect/fasta/ups1-ups2-yeast.fasta"
-# echo
-
-# cs true, fmdw true
-echo "P3830, cs true, fmdw true"
+echo "P3830, YeastUPS2, cs true, fmdw false"
 doit -f ./otf-peak-detect/pipeline/execute-run.py clean en=P3830
-doit -f ./otf-peak-detect/pipeline/execute-run.py pc=0.8 cs=true fmdw=true en=P3830 rn=P3830_YeastUPS1_01_Slot1-1_1_5066 rl=1600 ru=2000 ff="~/otf-peak-detect/fasta/ups1-ups2-yeast.fasta"
+doit -f ./otf-peak-detect/pipeline/execute-run.py pc=0.8 cs=true fmdw=false en=P3830 rn=P3830_YeastUPS2_01_Slot1-1_1_5082 rl=0 ru=3000 ff="~/otf-peak-detect/fasta/ups1-ups2-yeast.fasta"
 echo
 
-# cs false, fmdw true
-# echo "P3830, cs false, fmdw true"
-# doit -f ./otf-peak-detect/pipeline/execute-run.py clean en=P3830
-# doit -f ./otf-peak-detect/pipeline/execute-run.py pc=0.8 cs=false fmdw=true en=P3830 rn=P3830_YeastUPS1_01_Slot1-1_1_5066 rl=0 ru=3000 ff="~/otf-peak-detect/fasta/ups1-ups2-yeast.fasta"
-# echo
+echo "P3830, YeastUPS2, cs false, fmdw false"
+doit -f ./otf-peak-detect/pipeline/execute-run.py clean en=P3830
+doit -f ./otf-peak-detect/pipeline/execute-run.py pc=0.8 cs=false fmdw=false en=P3830 rn=P3830_YeastUPS2_01_Slot1-1_1_5082 rl=0 ru=3000 ff="~/otf-peak-detect/fasta/ups1-ups2-yeast.fasta"
+echo
+
+echo "P3830, YeastUPS1, cs true, fmdw false"
+doit -f ./otf-peak-detect/pipeline/execute-run.py clean en=P3830
+doit -f ./otf-peak-detect/pipeline/execute-run.py pc=0.8 cs=true fmdw=false en=P3830 rn=P3830_YeastUPS1_01_Slot1-1_1_5066 rl=0 ru=3000 ff="~/otf-peak-detect/fasta/ups1-ups2-yeast.fasta"
+echo
+
+echo "P3830, YeastUPS1, cs false, fmdw false"
+doit -f ./otf-peak-detect/pipeline/execute-run.py clean en=P3830
+doit -f ./otf-peak-detect/pipeline/execute-run.py pc=0.8 cs=false fmdw=false en=P3830 rn=P3830_YeastUPS1_01_Slot1-1_1_5066 rl=0 ru=3000 ff="~/otf-peak-detect/fasta/ups1-ups2-yeast.fasta"
+echo
 
 # P3856
 #
