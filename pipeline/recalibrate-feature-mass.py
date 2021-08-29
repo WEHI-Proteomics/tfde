3d4af93e742375c7689d986035511cf259517ed8
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2021-08-30 09:41:47 +1000

    Update recalibrate-feature-mass.py

--- pipeline/recalibrate-feature-mass.py
+++ pipeline/recalibrate-feature-mass.py
@@ -60,13 +60,15 @@ def generate_estimator(X_train, X_test, y_train, y_test):
 def adjust_features(run_name, idents_for_training_df, run_features_df):
     print("processing {} features for run {}".format(len(run_features_df), run_name))
 
-    X = idents_for_training_df[['monoisotopic_mz','scan_apex','rt_apex','feature_intensity']].to_numpy()
+    # X = idents_for_training_df[['monoisotopic_mz','scan_apex','rt_apex','feature_intensity']].to_numpy()
+    X = idents_for_training_df[['monoisotopic_mz','scan_apex','rt_apex']].to_numpy()
     y = idents_for_training_df[['mass_error']].to_numpy()[:,0]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
     best_estimator = generate_estimator(X_train, X_test, y_train, y_test)
 
     # use the trained model to predict the mass error for all the detected features
-    X = run_features_df[['monoisotopic_mz','scan_apex','rt_apex','feature_intensity']].to_numpy()
+    # X = run_features_df[['monoisotopic_mz','scan_apex','rt_apex','feature_intensity']].to_numpy()
+    X = run_features_df[['monoisotopic_mz','scan_apex','rt_apex']].to_numpy()
     y = best_estimator.predict(X)
 
     # collate the recalibrated feature attributes
