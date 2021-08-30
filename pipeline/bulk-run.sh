b5ab190b5ac23e9362f6c19705b37a0fbc906c3d
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2021-08-30 18:27:47 +1000

    Update bulk-run.sh

--- pipeline/bulk-run.sh
+++ pipeline/bulk-run.sh
@@ -24,22 +24,22 @@
 
 # P3856
 #
-echo "P3856, cs true, fmdw false"
-doit -f ./otf-peak-detect/pipeline/execute-run.py clean en=P3856
-doit -f ./otf-peak-detect/pipeline/execute-run.py pc=0.8 cs=true fmdw=false en=P3856 rn=P3856_YHE211_1_Slot1-1_1_5104 rl=1650 ru=2200 ff="/home/daryl/otf-peak-detect/fasta/Human_Yeast_Ecoli.fasta"
-echo
+# echo "P3856, cs true, fmdw false"
+# doit -f ./otf-peak-detect/pipeline/execute-run.py clean en=P3856
+# doit -f ./otf-peak-detect/pipeline/execute-run.py pc=0.8 cs=true fmdw=false en=P3856 rn=P3856_YHE211_1_Slot1-1_1_5104 rl=1650 ru=2200 ff="/home/daryl/otf-peak-detect/fasta/Human_Yeast_Ecoli.fasta"
+# echo
 
-echo "P3856, cs false, fmdw false"
-doit -f ./otf-peak-detect/pipeline/execute-run.py clean en=P3856
-doit -f ./otf-peak-detect/pipeline/execute-run.py pc=0.8 cs=false fmdw=false en=P3856 rn=P3856_YHE211_1_Slot1-1_1_5104 rl=1650 ru=2200 ff="/home/daryl/otf-peak-detect/fasta/Human_Yeast_Ecoli.fasta"
-echo
+# echo "P3856, cs false, fmdw false"
+# doit -f ./otf-peak-detect/pipeline/execute-run.py clean en=P3856
+# doit -f ./otf-peak-detect/pipeline/execute-run.py pc=0.8 cs=false fmdw=false en=P3856 rn=P3856_YHE211_1_Slot1-1_1_5104 rl=1650 ru=2200 ff="/home/daryl/otf-peak-detect/fasta/Human_Yeast_Ecoli.fasta"
+# echo
 
 echo "P3856, cs true, fmdw true"
 doit -f ./otf-peak-detect/pipeline/execute-run.py clean en=P3856
 doit -f ./otf-peak-detect/pipeline/execute-run.py pc=0.8 cs=true fmdw=true en=P3856 rn=P3856_YHE211_1_Slot1-1_1_5104 rl=1650 ru=2200 ff="/home/daryl/otf-peak-detect/fasta/Human_Yeast_Ecoli.fasta"
 echo
 
-echo "P3856, cs false, fmdw true"
-doit -f ./otf-peak-detect/pipeline/execute-run.py clean en=P3856
-doit -f ./otf-peak-detect/pipeline/execute-run.py pc=0.8 cs=false fmdw=true en=P3856 rn=P3856_YHE211_1_Slot1-1_1_5104 rl=1650 ru=2200 ff="/home/daryl/otf-peak-detect/fasta/Human_Yeast_Ecoli.fasta"
-echo
+# echo "P3856, cs false, fmdw true"
+# doit -f ./otf-peak-detect/pipeline/execute-run.py clean en=P3856
+# doit -f ./otf-peak-detect/pipeline/execute-run.py pc=0.8 cs=false fmdw=true en=P3856 rn=P3856_YHE211_1_Slot1-1_1_5104 rl=1650 ru=2200 ff="/home/daryl/otf-peak-detect/fasta/Human_Yeast_Ecoli.fasta"
+# echo
