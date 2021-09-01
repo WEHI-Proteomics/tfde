6b816629c7e8ef9fa68d363593101b79ee871fc2
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2021-09-01 15:45:45 +1000

    Update detect-features-with-3did.py

--- 3did/detect-features-with-3did.py
+++ 3did/detect-features-with-3did.py
@@ -800,14 +800,8 @@ features_df['feature_id'] = features_df.index
 
 # ... and save them in a file
 print()
-chunk_size = 1000
-num_chunks = len(features_df) // chunk_size
-if len(features_df) % chunk_size != 0:
-    num_chunks += 1
-print("writing {} features in {} chunks to {}".format(len(features_df), num_chunks, FEATURES_DIR))
-for i in range(num_chunks):
-    FEATURES_FILE = '{}/exp-{}-run-{}-features-3did-{:03d}.feather'.format(FEATURES_DIR, args.experiment_name, args.run_name, i)
-    features_df[i*chunk_size:(i + 1) * chunk_size].reset_index().to_feather(FEATURES_FILE)
+FEATURES_FILE = '{}/exp-{}-run-{}-features-3did.feather'.format(FEATURES_DIR, args.experiment_name, args.run_name)
+features_df.to_feather(FEATURES_FILE)
 
 # write the metadata
 info.append(('total_running_time',round(time.time()-start_run,1)))
