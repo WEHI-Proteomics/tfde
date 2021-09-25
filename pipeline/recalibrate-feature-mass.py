0f34944c85575e2b13451d8b1286dd83a2932678
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2021-09-25 11:44:50 +1000

    Update recalibrate-feature-mass.py

--- pipeline/recalibrate-feature-mass.py
+++ pipeline/recalibrate-feature-mass.py
@@ -159,17 +159,18 @@ del features_l[:]
 print('loaded {} features from {} files for recalibration'.format(len(features_df), len(feature_files)))
 
 # set up Ray
-print("setting up Ray")
-if not ray.is_initialized():
-    if args.ray_mode == "cluster":
-        ray.init(num_cpus=number_of_workers())
-    else:
-        ray.init(local_mode=True)
+# print("setting up Ray")
+# if not ray.is_initialized():
+#     if args.ray_mode == "cluster":
+#         ray.init(num_cpus=number_of_workers())
+#     else:
+#         ray.init(local_mode=True)
 
 # for each run, produce a model that estimates the mass error from a feature's characteristics, and generate a revised feature file with adjusted mass, 
 # to get a smaller mass error on a second Comet search with tighter mass tolerance.
 print("training models and adjusting monoisotopic mass for each feature")
-adjusted_features_l = ray.get([adjust_features.remote(run_name=group_name, idents_for_training_df=group_df, run_features_df=features_df[features_df.run_name == group_name][['run_name','feature_id','monoisotopic_mass','monoisotopic_mz','scan_apex','rt_apex','feature_intensity','charge']]) for group_name,group_df in idents_df.groupby('run_name')])
+# adjusted_features_l = ray.get([adjust_features.remote(run_name=group_name, idents_for_training_df=group_df, run_features_df=features_df[features_df.run_name == group_name][['run_name','feature_id','monoisotopic_mass','monoisotopic_mz','scan_apex','rt_apex','feature_intensity','charge']]) for group_name,group_df in idents_df.groupby('run_name')])
+adjusted_features_l = [adjust_features(run_name=group_name, idents_for_training_df=group_df, run_features_df=features_df[features_df.run_name == group_name][['run_name','feature_id','monoisotopic_mass','monoisotopic_mz','scan_apex','rt_apex','feature_intensity','charge']]) for group_name,group_df in idents_df.groupby('run_name')]
 
 # join the list of dataframes into a single dataframe
 adjusted_features_df = pd.concat(adjusted_features_l, axis=0, sort=False, ignore_index=True)
