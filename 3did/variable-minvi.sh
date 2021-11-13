1e587ac3eb2c4f7e7781f109dae4336244d8761f
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2021-11-14 09:30:57 +1100

    Update variable-minvi.sh

--- 3did/variable-minvi.sh
+++ 3did/variable-minvi.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-echo "P3856, minvi 2000"
-doit -f ./tfde/3did/execute.py clean en=P3856 rn=P3856_YHE211_1_Slot1-1_1_5104
-doit -f ./tfde/3did/execute.py pc=0.8 en=P3856 rdn=P3856_YHE211_1_Slot1-1_1_5104.d rn=P3856_YHE211_1_Slot1-1_1_5104 minvi=2000 mw=10
-echo
+# echo "P3856, minvi 2000"
+# doit -f ./tfde/3did/execute.py clean en=P3856 rn=P3856_YHE211_1_Slot1-1_1_5104
+# doit -f ./tfde/3did/execute.py pc=0.8 en=P3856 rdn=P3856_YHE211_1_Slot1-1_1_5104.d rn=P3856_YHE211_1_Slot1-1_1_5104 minvi=2000 mw=10
+# echo
 
-echo "P3856, minvi 2500"
-doit -f ./tfde/3did/execute.py clean en=P3856 rn=P3856_YHE211_1_Slot1-1_1_5104
-doit -f ./tfde/3did/execute.py pc=0.8 en=P3856 rdn=P3856_YHE211_1_Slot1-1_1_5104.d rn=P3856_YHE211_1_Slot1-1_1_5104 minvi=2500 mw=10
-echo
+# echo "P3856, minvi 2500"
+# doit -f ./tfde/3did/execute.py clean en=P3856 rn=P3856_YHE211_1_Slot1-1_1_5104
+# doit -f ./tfde/3did/execute.py pc=0.8 en=P3856 rdn=P3856_YHE211_1_Slot1-1_1_5104.d rn=P3856_YHE211_1_Slot1-1_1_5104 minvi=2500 mw=10
+# echo
 
 echo "P3856, minvi 3000"
 doit -f ./tfde/3did/execute.py clean en=P3856 rn=P3856_YHE211_1_Slot1-1_1_5104
 doit -f ./tfde/3did/execute.py pc=0.8 en=P3856 rdn=P3856_YHE211_1_Slot1-1_1_5104.d rn=P3856_YHE211_1_Slot1-1_1_5104 minvi=3000 mw=10
 echo
 
-echo "P3856, minvi 4000"
-doit -f ./tfde/3did/execute.py clean en=P3856 rn=P3856_YHE211_1_Slot1-1_1_5104
-doit -f ./tfde/3did/execute.py pc=0.8 en=P3856 rdn=P3856_YHE211_1_Slot1-1_1_5104.d rn=P3856_YHE211_1_Slot1-1_1_5104 minvi=4000 mw=10
-echo
+# echo "P3856, minvi 4000"
+# doit -f ./tfde/3did/execute.py clean en=P3856 rn=P3856_YHE211_1_Slot1-1_1_5104
+# doit -f ./tfde/3did/execute.py pc=0.8 en=P3856 rdn=P3856_YHE211_1_Slot1-1_1_5104.d rn=P3856_YHE211_1_Slot1-1_1_5104 minvi=4000 mw=10
+# echo
 
-echo "P3856, minvi 5000"
-doit -f ./tfde/3did/execute.py clean en=P3856 rn=P3856_YHE211_1_Slot1-1_1_5104
-doit -f ./tfde/3did/execute.py pc=0.8 en=P3856 rdn=P3856_YHE211_1_Slot1-1_1_5104.d rn=P3856_YHE211_1_Slot1-1_1_5104 minvi=5000 mw=10
-echo
+# echo "P3856, minvi 5000"
+# doit -f ./tfde/3did/execute.py clean en=P3856 rn=P3856_YHE211_1_Slot1-1_1_5104
+# doit -f ./tfde/3did/execute.py pc=0.8 en=P3856 rdn=P3856_YHE211_1_Slot1-1_1_5104.d rn=P3856_YHE211_1_Slot1-1_1_5104 minvi=5000 mw=10
+# echo
 
-echo "P3856, minvi 6000"
-doit -f ./tfde/3did/execute.py clean en=P3856 rn=P3856_YHE211_1_Slot1-1_1_5104
-doit -f ./tfde/3did/execute.py pc=0.8 en=P3856 rdn=P3856_YHE211_1_Slot1-1_1_5104.d rn=P3856_YHE211_1_Slot1-1_1_5104 minvi=6000 mw=10
-echo
+# echo "P3856, minvi 6000"
+# doit -f ./tfde/3did/execute.py clean en=P3856 rn=P3856_YHE211_1_Slot1-1_1_5104
+# doit -f ./tfde/3did/execute.py pc=0.8 en=P3856 rdn=P3856_YHE211_1_Slot1-1_1_5104.d rn=P3856_YHE211_1_Slot1-1_1_5104 minvi=6000 mw=10
+# echo
 
-echo "P3856, minvi 7000"
-doit -f ./tfde/3did/execute.py clean en=P3856 rn=P3856_YHE211_1_Slot1-1_1_5104
-doit -f ./tfde/3did/execute.py pc=0.8 en=P3856 rdn=P3856_YHE211_1_Slot1-1_1_5104.d rn=P3856_YHE211_1_Slot1-1_1_5104 minvi=7000 mw=10
-echo
+# echo "P3856, minvi 7000"
+# doit -f ./tfde/3did/execute.py clean en=P3856 rn=P3856_YHE211_1_Slot1-1_1_5104
+# doit -f ./tfde/3did/execute.py pc=0.8 en=P3856 rdn=P3856_YHE211_1_Slot1-1_1_5104.d rn=P3856_YHE211_1_Slot1-1_1_5104 minvi=7000 mw=10
+# echo
