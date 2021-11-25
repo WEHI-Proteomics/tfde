00414d6b1f5e34eb2082196a3e47a54ca269afbc
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2021-11-25 18:27:03 +1100

    Update identify-searched-features.py

--- pipeline/identify-searched-features.py
+++ pipeline/identify-searched-features.py
@@ -156,8 +156,7 @@ identifications_df['mass_error'] = identifications_df['observed_monoisotopic_mas
 print('counting unique peptides')
 sequences_l = []
 for group_name,group_df in identifications_df.groupby(['sequence','charge'], as_index=False):
-    if group_df['percolator q-value'].min() <= MAXIMUM_Q_VALUE:
-        sequences_l.append({'sequence_charge':group_name, 'feature_ids':group_df.feature_id.tolist()})
+    sequences_l.append({'sequence_charge':group_name, 'feature_ids':group_df.feature_id.tolist()})
 sequences_df = pd.DataFrame(sequences_l)
 print('there were {} unique peptides identified with q-value less than {}'.format(len(sequences_df), MAXIMUM_Q_VALUE))
 
