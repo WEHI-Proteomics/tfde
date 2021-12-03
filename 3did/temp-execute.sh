4add6d2f3ec5b2f4ad725ae6b2ba8368f0a3136d
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2021-12-04 08:06:25 +1100

    Update temp-execute.sh

--- 3did/temp-execute.sh
+++ 3did/temp-execute.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-python -u ./tfde/pipeline/remove-duplicate-features.py -eb /media/big-ssd/experiments -en P3856_YHE211 -rn P3856_YHE211_1_Slot1-1_1_5104 -pdm 3did
 python -u ./tfde/pipeline/render-features-as-mgf.py -eb /media/big-ssd/experiments -en P3856_YHE211 -rn P3856_YHE211_1_Slot1-1_1_5104 -pdm 3did
 python -u ./tfde/pipeline/search-mgf-against-sequence-db.py -eb /media/big-ssd/experiments -en P3856_YHE211 -rn P3856_YHE211_1_Slot1-1_1_5104 -ff ./tfde/fasta/Human_Yeast_Ecoli.fasta -pdm 3did
 python -u ./tfde/pipeline/identify-searched-features.py -eb /media/big-ssd/experiments -en P3856_YHE211 -ff ./tfde/fasta/Human_Yeast_Ecoli.fasta -pdm 3did
