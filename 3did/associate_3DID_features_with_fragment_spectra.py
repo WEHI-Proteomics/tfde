751778177d63b1466e13ef409793859a2b968d16
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2021-12-04 12:22:13 +1100

    Update associate_3DID_features_with_fragment_spectra.py

--- 3did/associate_3DID_features_with_fragment_spectra.py
+++ 3did/associate_3DID_features_with_fragment_spectra.py
@@ -133,7 +133,7 @@ def resolve_fragment_ions(feature_charge, ms2_points_df):
     deconvoluted_peaks_df.intensity = deconvoluted_peaks_df.intensity.astype(np.uint)
     deconvoluted_peaks_df = deconvoluted_peaks_df[(deconvoluted_peaks_df.intensity > 0)]
     
-    return deconvoluted_peaks_df.head(n=30)
+    return deconvoluted_peaks_df.head(n=50)
 
 @ray.remote
 def process_cuboid_features(cuboid):
