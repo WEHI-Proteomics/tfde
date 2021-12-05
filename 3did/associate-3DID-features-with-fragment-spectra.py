f2c135ddfa6609f3ea8691f2b666e0a2ee0a59b9
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2021-12-05 12:26:51 +1100

    Update associate-3DID-features-with-fragment-spectra.py

--- 3did/associate-3DID-features-with-fragment-spectra.py
+++ 3did/associate-3DID-features-with-fragment-spectra.py
@@ -27,7 +27,7 @@ precursor_cuboids_df = pd.read_feather(precursor_cuboids_name)
 
 tdid_experiment_name = 'P3856_YHE211'
 tdid_results_name = 'minvi-1000-2021-12-05-01-06-49'
-features_dir = '/media/big-ssd/results-{}-3did/{}/features-3did'.format(tdid_experiment_name, tdid_results_name)
+features_dir = '/media/big-ssd/results-{}-3did/{}/{}/features-3did'.format(tdid_experiment_name, tdid_results_name, tdid_experiment_name)
 features_file = '{}/exp-{}-run-{}-features-3did-dedup.feather'.format(features_dir, tdid_experiment_name, run_name)
 output_features_file = '{}/exp-{}-run-{}-features-3did-assoc.feather'.format(features_dir, tdid_experiment_name, run_name)
 print('loading features from {}'.format(features_file))
