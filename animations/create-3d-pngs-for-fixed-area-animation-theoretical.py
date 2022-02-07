a87896eff79ea1975b2743ab09254e6b45089055
Daryl Wilding-McBride <daryl.wmcb@gmail.com>
2022-02-07 12:47:00 +1100

    Update create-3d-pngs-for-fixed-area-animation-theoretical.py

--- animations/create-3d-pngs-for-fixed-area-animation-theoretical.py
+++ animations/create-3d-pngs-for-fixed-area-animation-theoretical.py
@@ -26,12 +26,12 @@ azimuth = 230
 frame_counter = 0
 
 # m/z extent
-mz_lower = 695
-mz_upper = 706
+mz_lower = 698
+mz_upper = 704
 
 # CCS extent
-scan_lower = 500
-scan_upper = 800
+scan_lower = 530
+scan_upper = 770
 
 # RT extent
 rt_lower = 200
@@ -42,7 +42,8 @@ rt_values = np.arange(rt_lower, rt_upper, 1.0)
 ccs_values = np.arange(scan_lower, scan_upper, 1.0)
 
 peak_rt = ((rt_upper-rt_lower)/2)+rt_lower
-feature_intensity_values = func(rt_values, 1.0, peak_rt, 5.0)
+std_dev_rt = 10
+feature_intensity_values = func(rt_values, 1.0, peak_rt, std_dev_rt)
 
 for frame_id,rt in enumerate(rt_values):
     print("rendering frame {}".format(frame_counter))
@@ -76,20 +77,49 @@ for frame_id,rt in enumerate(rt_values):
     mz = 700.0
     mz_values = np.zeros((len(ccs_values),), dtype=float)+mz
 
+    intensity_threshold = 0.001
+
     peak_ccs = ((scan_upper - scan_lower) / 2) + scan_lower
-    std_dev = 10
+    std_dev_ccs = 20
 
-    iso_1 = func(ccs_values, feature_intensity_values[frame_id]*1.0, peak_ccs, std_dev)
-    iso_2 = func(ccs_values, feature_intensity_values[frame_id]*0.6, peak_ccs, std_dev)
-    iso_3 = func(ccs_values, feature_intensity_values[frame_id]*0.4, peak_ccs, std_dev)
-    iso_4 = func(ccs_values, feature_intensity_values[frame_id]*0.2, peak_ccs, std_dev)
-    iso_5 = func(ccs_values, feature_intensity_values[frame_id]*0.1, peak_ccs, std_dev)
+    iso_intensity_values = feature_intensity_values[frame_id]*1.0
+    x = mz_values
+    y = ccs_values
+    z = func(y, iso_intensity_values, peak_ccs, std_dev_ccs)
+    idx = z > intensity_threshold
+    ax.scatter(x[idx], y[idx], z[idx], s=4**2, c=z[idx], cmap=plt.get_cmap('cool'), alpha=1.0)
 
-    ax.scatter(mz_values, ccs_values, iso_1, s=4**2, c=iso_1, cmap=plt.get_cmap('cool'), alpha=1.0)
-    ax.scatter(mz_values+0.5, ccs_values, iso_2, s=4**2, c=iso_2, cmap=plt.get_cmap('cool'), alpha=1.0)
-    ax.scatter(mz_values+1.0, ccs_values, iso_3, s=4**2, c=iso_3, cmap=plt.get_cmap('cool'), alpha=1.0)
-    ax.scatter(mz_values+1.5, ccs_values, iso_4, s=4**2, c=iso_4, cmap=plt.get_cmap('cool'), alpha=1.0)
-    ax.scatter(mz_values+2.0, ccs_values, iso_5, s=4**2, c=iso_5, cmap=plt.get_cmap('cool'), alpha=1.0)
+    iso_intensity_values = feature_intensity_values[frame_id]*0.8
+    nonzero_idx = iso_intensity_values > intensity_threshold
+    x = mz_values+0.5
+    y = ccs_values
+    z = func(y, iso_intensity_values, peak_ccs, std_dev_ccs)
+    idx = z > intensity_threshold
+    ax.scatter(x[idx], y[idx], z[idx], s=4**2, c=z[idx], cmap=plt.get_cmap('cool'), alpha=1.0)
+
+    iso_intensity_values = feature_intensity_values[frame_id]*0.4
+    nonzero_idx = iso_intensity_values > intensity_threshold
+    x = mz_values+1.0
+    y = ccs_values
+    z = func(y, iso_intensity_values, peak_ccs, std_dev_ccs)
+    idx = z > intensity_threshold
+    ax.scatter(x[idx], y[idx], z[idx], s=4**2, c=z[idx], cmap=plt.get_cmap('cool'), alpha=1.0)
+
+    iso_intensity_values = feature_intensity_values[frame_id]*0.2
+    nonzero_idx = iso_intensity_values > intensity_threshold
+    x = mz_values+1.5
+    y = ccs_values
+    z = func(y, iso_intensity_values, peak_ccs, std_dev_ccs)
+    idx = z > intensity_threshold
+    ax.scatter(x[idx], y[idx], z[idx], s=4**2, c=z[idx], cmap=plt.get_cmap('cool'), alpha=1.0)
+
+    iso_intensity_values = feature_intensity_values[frame_id]*0.1
+    nonzero_idx = iso_intensity_values > intensity_threshold
+    x = mz_values+2.0
+    y = ccs_values
+    z = func(y, iso_intensity_values, peak_ccs, std_dev_ccs)
+    idx = z > intensity_threshold
+    ax.scatter(x[idx], y[idx], z[idx], s=4**2, c=z[idx], cmap=plt.get_cmap('cool'), alpha=1.0)
 
     # fig.suptitle('frame id {}, retention time (secs) {}'.format(frame_id, round(frame_df.iloc[0].retention_time_secs, 1)), fontsize=16, x=0.5, y=0.85)
     plt.savefig('{}/img-{:04d}.png'.format(working_folder, frame_counter), bbox_inches='tight', facecolor=fig.get_facecolor())
